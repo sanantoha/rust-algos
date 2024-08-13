a6170634b3fef9c40315052d226f9e2900936f5a
AntonSakharovAE <anton@alefeducation.com>
2024-08-13 14:00:00 +0400

    add bfs

---
+++ src/tree/bfs_tree_traverse.rs
@@ -0,0 +1,54 @@
+use std::collections::LinkedList;
+use crate::tree::tree::TreeNode;
+
+// O(n) time | O(n) space
+pub fn bfs(root: &Option<Box<TreeNode>>) -> Vec<i32> {
+    if let Some(node) = root {
+
+        let mut queue = LinkedList::new();
+        queue.push_back(node);
+
+        let mut res = vec![];
+
+        while !queue.is_empty() {
+            if let Some(curr) = queue.pop_front() {
+                res.push(curr.val);
+
+                if let Some(left) = &curr.left {
+                    queue.push_back(left);
+                }
+
+                if let Some(right) = &curr.right {
+                    queue.push_back(right);
+                }
+            }
+        }
+        return res;
+    } else {
+        return vec![];
+    }
+}
+
+#[test]
+pub fn test_bfs() {
+
+    let root = Some(Box::new(TreeNode::new(
+        5,
+        Some(Box::new(TreeNode::new(
+            2,
+            Some(Box::new(TreeNode::single(1))),
+            Some(Box::new(TreeNode::single(3))),
+        ))),
+        Some(Box::new(TreeNode::new(
+            10,
+            Some(Box::new(TreeNode::single(7))),
+            Some(Box::new(TreeNode::new(
+                15,
+                Some(Box::new(TreeNode::single(13))),
+                Some(Box::new(TreeNode::single(17))),
+            ))),
+        ))),
+    )));
+
+    assert_eq!(bfs(&root), vec![5, 2, 10, 1, 3, 7, 15, 13, 17]);
+}
--- src/tree/mod.rs
+++ src/tree/mod.rs
@@ -2,3 +2,4 @@ pub mod tree;
 pub mod balanced_binary_tree;
 pub mod dfs_tree_traverse_rec;
 pub mod dfs_tree_traverse;
+pub mod bfs_tree_traverse;
