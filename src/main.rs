d6479cb91cfc0e11972c15b34d6c3f069f8f7691
AntonSakharovAE <anton@alefeducation.com>
2024-11-17 21:05:49 +0400

    random function

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,96 @@
-use std::rc::Rc;
-use rust_algos::tree;
-use tree::TreeNode;
-use rust_algos::list;
-use list::ListNode;
-use rust_algos::list::Displayable;
+use std::error::Error;
+use std::fs::{DirEntry, FileType};
+use std::fs::OpenOptions;
+use std::io::{BufRead, BufReader};
+use rand::prelude::SliceRandom;
 
 fn main() {
+    let base_path = "./src/";
+    let completed_path = "./completed.txt";
 
-    let mut root = TreeNode::leaf(1);
+    let mut rnd = rand::thread_rng();
 
-    // Replace the empty children with real nodes
-    root.left = Some(Box::new(TreeNode::leaf(2)));
-    root.right = Some(Box::new(TreeNode::leaf(3)));
+    if let (Ok(mut source), Ok(completed)) = (get_all_files(base_path), read_completed(completed_path)) {
+        source.retain(|path| !completed.contains(path));
 
-    println!("Tree {:?}", root);
+        source.shuffle(&mut rnd);
 
-    let third = ListNode::new(3);
-    let second = ListNode::with_next(2, Some(third));
-    let first = ListNode::with_next(1, Some(second));
-
-    let head = ListNode::with_next(1, Some(ListNode::with_next(2, Some(ListNode::with_next(3, Some(ListNode::with_next(4, Some(ListNode::new(5)))))))));
-
-    let ffirst = Rc::clone(&first);
-    let fffirst = Rc::clone(&first);
-
-    println!("{} | {}", Displayable::new(Rc::clone(&ffirst)), Displayable::new(fffirst));
-    println!("{}", Displayable::new(head));
-
-    // println!("{}", Rc::strong_count(&ffirst));
-
-    let s = first.borrow().next.as_ref().map(Rc::clone);
-
-    println!("{}", Rc::strong_count(&first));
-
-    if let Some(sss) = Displayable::from_option(s) {
-        println!("{}", sss);
+        for path in source {
+            println!("{}", path);
+        }
     }
+}
+
+fn read_completed(path: &str) -> Result<Vec<String>, Box<dyn Error>> {
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(path)?;
+
+    let metadata = file.metadata()?;
+    if metadata.len() == 0 {
+        return Ok(vec![]);
+    }
+
+    let reader = BufReader::new(file);
+    let mut res  = vec![];
+    for line in reader.lines() {
+        match line {
+            Ok(content) => res.push(content.trim().to_string()),
+            Err(e) => {
+                eprintln!("Error reading line: {}", e);
+                return Err(e.into());
+            },
+        }
+    }
+
+    Ok(res)
+}
+
+fn get_all_files(base_path: &str) -> Result<Vec<String>, Box<dyn Error>> {
+    let dirs = list(base_path, directories)?;
+
+    let mut res = vec![];
+
+    for dir in dirs {
+        let path = format!("{}{}", base_path, dir);
+        let files = list(&path, files)?;
+        for file in files {
+            res.push(file);
+        }
+    }
+
+    Ok(res)
+}
+
+fn files(file_type: FileType, entry: DirEntry, res: &mut Vec<String>) {
+    if file_type.is_file() {
+        if let Ok(name) = entry.file_name().into_string() {
+            res.push(name);
+        }
+    }
+}
+
+fn directories(file_type: FileType, entry: DirEntry, res: &mut Vec<String>) {
+    if file_type.is_dir() {
+        if let Ok(name) = entry.file_name().into_string() {
+            res.push(name);
+        }
+    }
+}
+
+fn list<F>(path: &str, f: F) -> Result<Vec<String>, Box<dyn Error>>
+where F: Fn(FileType, DirEntry, &mut Vec<String>) -> ()
+{
+    let mut res = vec![];
+
+    for entry in std::fs::read_dir(path)? {
+        let entry = entry?;
+        let file_type = entry.file_type()?;
+
+        f(file_type, entry, &mut res);
+    }
+
+    Ok(res)
 }
