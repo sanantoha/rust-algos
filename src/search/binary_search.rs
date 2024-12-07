1a7620859323944a7ce4e2f2ea4f05ed4896c33d
AntonSakharovAE <anton@alefeducation.com>
2024-12-07 12:07:13 +0400

    refactor binary search

--- src/search/binary_search.rs
+++ src/search/binary_search.rs
@@ -1,20 +1,20 @@
 
 // O(log(n)) time | O(1) space
 pub fn binary_search(arr: &[i32], target: i32) -> i32 {
-    let mut l: usize = 0;
-    let mut r: usize = arr.len();
+    let mut l: i32 = 0;
+    let mut r: i32 = arr.len() as i32 - 1;
 
     while l <= r {
         let mid = l + (r - l) / 2;
-        if target < arr[mid] {
+        if target < arr[mid as usize] {
             r = mid - 1;
-        } else if target > arr[mid] {
+        } else if target > arr[mid as usize] {
             l = mid + 1;
         } else {
-            return mid as i32;
+            return mid;
         }
     }
-    -((l + 1) as i32)
+    -(l + 1)
 }
 
 
