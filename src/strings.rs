f420bb10f542ea665c27349a7e9ba9e9906c3bfc
AntonSakharovAE <anton@alefeducation.com>
2025-02-09 13:27:48 +0400

    rename strings.rs

--- src/strings.rs
+++ src/strings.rs
@@ -2,6 +2,6 @@ pub mod reverse_string;
 mod one_edit;
 mod palindromic_strings;
 mod reverse_integer;
-mod reverse_wors_in_string;
+mod reverse_words_in_string;
 mod string_without_aaa_or_bbb;
 mod valid_ip_addresses;
