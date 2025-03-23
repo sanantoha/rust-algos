70ab9bf3ae5f8542bba0a95180d0af65fa6e1042
AntonSakharovAE <anton@alefeducation.com>
2025-03-23 10:48:01 +0400

    add one task

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,48 +1,57 @@
-use crate::tree::TreeNode;
+use std::collections::HashMap;
+use std::rc::Rc;
+use crate::graph::EdgeT;
 
-pub fn pre_order_rec(root: &Option<Box<TreeNode>>) -> Vec<i32> {
-    vec![]
+
+pub fn shortest_paths(graph: &HashMap<String, Vec<Rc<EdgeT<String>>>>, start: String) -> (HashMap<String, f64>, HashMap<String, String>) {
+
+    (HashMap::default(), HashMap::default())
 }
-
-pub fn in_order_rec(root: &Option<Box<TreeNode>>) -> Vec<i32> {
-    vec![]
-}
-
-pub fn post_order_rec(root: &Option<Box<TreeNode>>) -> Vec<i32> {
-    vec![]
-}
-
 
 #[cfg(test)]
 mod tests {
-    use super::{pre_order_rec, in_order_rec, post_order_rec};
-    use crate::tree::TreeNode;
+    use std::collections::HashMap;
+    use std::path::PathBuf;
+    use crate::graph::{graph_from_file, graph_to_string};
+    use super::shortest_paths;
 
+    // {"0": 0.0, "1": 5.0, "4": 7.0, "3": 4.0, "2": 8.0}, {"1": "3", "4": "3", "0": "", "2": "1", "3": "0"}
     #[test]
-    pub fn test_dfs_pre_order_rec() {
-        assert_eq!(pre_order_rec(&create_tree()), vec![5, 2, 1, 3, 8, 7, 9]);
-    }
+    fn test_shortest_paths() {
+        if let Ok(graph) = graph_from_file(PathBuf::from("src/graph/dijkstraShortestPath.txt")) {
+            println!("{}", graph_to_string(&graph));
 
-    #[test]
-    pub fn test_dfs_in_order_rec() {
-        assert_eq!(in_order_rec(&create_tree()), vec![1, 2, 3, 5, 7, 8, 9]);
-    }
+            let mut exp_prev = HashMap::new();
+            exp_prev.insert(String::from("0"), String::from(""));
+            exp_prev.insert(String::from("1"), String::from("3"));
+            exp_prev.insert(String::from("2"), String::from("1"));
+            exp_prev.insert(String::from("3"), String::from("0"));
+            exp_prev.insert(String::from("4"), String::from("3"));
 
-    #[test]
-    pub fn test_dfs_post_order_rec() {
-        assert_eq!(post_order_rec(&create_tree()), vec![1, 3, 2, 7, 9, 8, 5]);
-    }
+            let mut exp_shortest = HashMap::new();
+            exp_shortest.insert(String::from("0"), 0.0);
+            exp_shortest.insert(String::from("1"), 5.0);
+            exp_shortest.insert(String::from("2"), 8.0);
+            exp_shortest.insert(String::from("3"), 4.0);
+            exp_shortest.insert(String::from("4"), 7.0);
 
-    fn create_tree() -> Option<Box<TreeNode>> {
-        Some(
-            Box::new(TreeNode::new(5,
-                                   Some(Box::new(TreeNode::new(2,
-                                                               Some(Box::new(TreeNode::leaf(1))),
-                                                               Some(Box::new(TreeNode::leaf(3)))))),
-                                   Some(Box::new(TreeNode::new(8,
-                                                                              Some(Box::new(TreeNode::leaf(7))),
-                                                                              Some(Box::new(TreeNode::leaf(9)))))),
-            ))
-        )
+            let (shortest, prev) = shortest_paths(&graph, "0".to_string());
+            println!("{:?}", shortest);
+            println!("{:?}", prev);
+
+            assert_eq!(prev.len(), exp_prev.len());
+            for (k, v) in &prev {
+                if let Some(v2) = exp_prev.get(k) {
+                    assert_eq!(v, v2, "{}", k);
+                }
+            }
+
+            assert_eq!(shortest.len(), exp_shortest.len());
+            for (k, v) in &shortest {
+                if let Some(v2) = exp_shortest.get(k) {
+                    assert_eq!(v, v2, "{}", k);
+                }
+            }
+        }
     }
 }
