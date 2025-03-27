d8952460541426e36bbd86d5181d72a001fe33f5
AntonSakharovAE <anton@alefeducation.com>
2025-03-27 10:19:43 +0400

    add 1 task.

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,25 +1,56 @@
+use std::collections::HashMap;
+use std::rc::Rc;
+use crate::graph::EdgeT;
 
-pub fn generate_matrix(n: usize) -> Vec<Vec<i32>> {
-    vec![]
+pub fn shortest_paths(graph: &HashMap<String, Vec<Rc<EdgeT<String>>>>, start: String) -> (HashMap<String, f64>, HashMap<String, String>) {
+    (HashMap::default(), HashMap::default())
 }
 
 
 #[cfg(test)]
 mod tests {
-    use super::generate_matrix;
+    use std::collections::HashMap;
+    use std::path::PathBuf;
+    use crate::graph::{graph_from_file, graph_to_string};
+    use super::shortest_paths;
 
+    // {"0": 0.0, "1": 5.0, "4": 7.0, "3": 4.0, "2": 8.0}, {"1": "3", "4": "3", "0": "", "2": "1", "3": "0"}
     #[test]
-    fn test_generate_matrix_for_3() {
-        assert_eq!(generate_matrix(3), vec![vec![1, 2, 3], vec![8, 9, 4], vec![7, 6, 5]])
-    }
+    fn test_shortest_paths() {
+        if let Ok(graph) = graph_from_file(PathBuf::from("src/graph/dijkstraShortestPath.txt")) {
+            println!("{}", graph_to_string(&graph));
 
-    #[test]
-    fn test_generate_matrix_for_1() {
-        assert_eq!(generate_matrix(1), vec![vec![1]])
-    }
+            let mut exp_prev = HashMap::new();
+            exp_prev.insert(String::from("0"), String::from(""));
+            exp_prev.insert(String::from("1"), String::from("3"));
+            exp_prev.insert(String::from("2"), String::from("1"));
+            exp_prev.insert(String::from("3"), String::from("0"));
+            exp_prev.insert(String::from("4"), String::from("3"));
 
-    #[test]
-    fn test_generate_matrix_for_4() {
-        assert_eq!(generate_matrix(4), vec![vec![1, 2, 3, 4], vec![12, 13, 14, 5], vec![11, 16, 15, 6], vec![10, 9, 8, 7]])
+            let mut exp_shortest = HashMap::new();
+            exp_shortest.insert(String::from("0"), 0.0);
+            exp_shortest.insert(String::from("1"), 5.0);
+            exp_shortest.insert(String::from("2"), 8.0);
+            exp_shortest.insert(String::from("3"), 4.0);
+            exp_shortest.insert(String::from("4"), 7.0);
+
+            let (shortest, prev) = shortest_paths(&graph, "0".to_string());
+            println!("{:?}", shortest);
+            println!("{:?}", prev);
+
+            assert_eq!(prev.len(), exp_prev.len());
+            for (k, v) in &prev {
+                if let Some(v2) = exp_prev.get(k) {
+                    assert_eq!(v, v2, "{}", k);
+                }
+            }
+
+            assert_eq!(shortest.len(), exp_shortest.len());
+            for (k, v) in &shortest {
+                if let Some(v2) = exp_shortest.get(k) {
+                    assert_eq!(v, v2, "{}", k);
+                }
+            }
+        }
     }
 }
