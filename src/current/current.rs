6b3e5805050c2e816642499592455de226d3ed57
AntonSakharovAE <anton@alefeducation.com>
2025-04-07 09:07:40 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,40 +1,81 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+use crate::tree::Node;
 
-
-pub fn counting_sort(arr: &mut [i32]) {
-
+pub fn connect(root: Option<Rc<RefCell<Node>>>) -> Option<Rc<RefCell<Node>>> {
+    None
 }
-
 
 #[cfg(test)]
 mod tests {
-    use rand::{thread_rng, Rng};
-    use super::*;
+
+    use super::connect;
+    use std::cell::RefCell;
+    use std::rc::Rc;
+    use crate::tree::Node;
 
     #[test]
-    fn test_counting_sort() {
-        let mut arr = vec![5, 2, 4, 6, 1, 3];
+    fn test_connect() {
 
-        counting_sort(&mut arr);
+        let root = Some(Rc::new(RefCell::new(Node::new_with(1,
+                                                            Some(Rc::new(RefCell::new(Node::new_with(2,
+                                                                                                     Some(Rc::new(RefCell::new(Node::new(4)))),
+                                                                                                     Some(Rc::new(RefCell::new(Node::new(5)))),
+                                                                                                     None
+                                                            )))),
+                                                            Some(Rc::new(RefCell::new(Node::new_with(3,
+                                                                                                     Some(Rc::new(RefCell::new(Node::new(6)))),
+                                                                                                     Some(Rc::new(RefCell::new(Node::new(7)))),
+                                                                                                     None
+                                                            )))),
+                                                            None
+        ))));
 
-        assert_eq!(arr, [1, 2, 3, 4, 5, 6]);
+
+        let exp = create_exp_tree();
+
+        let res = connect(root.as_ref().map(Rc::clone));
+        assert_eq!(res, exp);
     }
 
-    #[test]
-    fn test_counting_sort_case1() {
-        let mut arr = vec![0; 30];
+    fn create_exp_tree() -> Option<Rc<RefCell<Node>>> {
+        let node2 = Some(Rc::new(RefCell::new(Node::new(2))));
+        let node4 = Some(Rc::new(RefCell::new(Node::new(4))));
+        let node5 = Some(Rc::new(RefCell::new(Node::new(5))));
 
-        let mut rand = thread_rng();
+        let node3 = Some(Rc::new(RefCell::new(Node::new(3))));
+        let node6 = Some(Rc::new(RefCell::new(Node::new(6))));
+        let node7 = Some(Rc::new(RefCell::new(Node::new(7))));
 
-        for i in 0..arr.len() {
-            arr[i] = rand.gen_range(0..100)
+        if let Some(node2_) = node2.as_ref() {
+            node2_.borrow_mut().parent = node3.as_ref().map(Rc::clone);
+            node2_.borrow_mut().left = node4.as_ref().map(Rc::clone);
+            node2_.borrow_mut().right = node5.as_ref().map(Rc::clone);
         }
 
-        counting_sort(&mut arr);
-
-        println!("{:?}", arr);
-
-        for i in 1..arr.len() {
-            assert!(arr[i - 1] <= arr[i]);
+        if let Some(node3_) = node3.as_ref() {
+            node3_.borrow_mut().left = node6.as_ref().map(Rc::clone);
+            node3_.borrow_mut().right = node7.as_ref().map(Rc::clone);
         }
+
+        if let Some(node4_) = node4.as_ref() {
+            node4_.borrow_mut().parent = node5.as_ref().map(Rc::clone);
+        }
+
+        if let Some(node5_) = node5.as_ref() {
+            node5_.borrow_mut().parent = node6.as_ref().map(Rc::clone);
+        }
+
+        if let Some(node6_) = node6.as_ref() {
+            node6_.borrow_mut().parent = node7.as_ref().map(Rc::clone);
+        }
+
+        let exp = Some(Rc::new(RefCell::new(Node::new_with(1,
+                                                           node2.as_ref().map(Rc::clone),
+                                                           node3.as_ref().map(Rc::clone),
+                                                           None
+        ))));
+
+        exp
     }
 }
