79075bc98253f193da9715338431b86327e0e095
AntonSakharovAE <anton@alefeducation.com>
2025-04-08 10:40:21 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,81 +1,28 @@
-use std::cell::RefCell;
-use std::rc::Rc;
-use crate::tree::Node;
+use std::collections::HashMap;
 
-pub fn connect(root: Option<Rc<RefCell<Node>>>) -> Option<Rc<RefCell<Node>>> {
-    None
+
+pub fn optimal_freelancing(jobs: &mut Vec<HashMap<&str, i32>>) -> i32 {
+    0
 }
 
 #[cfg(test)]
 mod tests {
-
-    use super::connect;
-    use std::cell::RefCell;
-    use std::rc::Rc;
-    use crate::tree::Node;
+    use super::optimal_freelancing;
+    use std::collections::HashMap;
 
     #[test]
-    fn test_connect() {
+    fn it_optimal_freelancing() {
 
-        let root = Some(Rc::new(RefCell::new(Node::new_with(1,
-                                                            Some(Rc::new(RefCell::new(Node::new_with(2,
-                                                                                                     Some(Rc::new(RefCell::new(Node::new(4)))),
-                                                                                                     Some(Rc::new(RefCell::new(Node::new(5)))),
-                                                                                                     None
-                                                            )))),
-                                                            Some(Rc::new(RefCell::new(Node::new_with(3,
-                                                                                                     Some(Rc::new(RefCell::new(Node::new(6)))),
-                                                                                                     Some(Rc::new(RefCell::new(Node::new(7)))),
-                                                                                                     None
-                                                            )))),
-                                                            None
-        ))));
+        let mut jobs: Vec<HashMap<&str, i32>> = vec![
+            HashMap::from([("deadline", 2), ("payment", 2)]),
+            HashMap::from([("deadline", 4), ("payment", 3)]),
+            HashMap::from([("deadline", 5), ("payment", 1)]),
+            HashMap::from([("deadline", 7), ("payment", 2)]),
+            HashMap::from([("deadline", 3), ("payment", 1)]),
+            HashMap::from([("deadline", 3), ("payment", 2)]),
+            HashMap::from([("deadline", 1), ("payment", 3)]),
+        ];
 
-
-        let exp = create_exp_tree();
-
-        let res = connect(root.as_ref().map(Rc::clone));
-        assert_eq!(res, exp);
-    }
-
-    fn create_exp_tree() -> Option<Rc<RefCell<Node>>> {
-        let node2 = Some(Rc::new(RefCell::new(Node::new(2))));
-        let node4 = Some(Rc::new(RefCell::new(Node::new(4))));
-        let node5 = Some(Rc::new(RefCell::new(Node::new(5))));
-
-        let node3 = Some(Rc::new(RefCell::new(Node::new(3))));
-        let node6 = Some(Rc::new(RefCell::new(Node::new(6))));
-        let node7 = Some(Rc::new(RefCell::new(Node::new(7))));
-
-        if let Some(node2_) = node2.as_ref() {
-            node2_.borrow_mut().parent = node3.as_ref().map(Rc::clone);
-            node2_.borrow_mut().left = node4.as_ref().map(Rc::clone);
-            node2_.borrow_mut().right = node5.as_ref().map(Rc::clone);
-        }
-
-        if let Some(node3_) = node3.as_ref() {
-            node3_.borrow_mut().left = node6.as_ref().map(Rc::clone);
-            node3_.borrow_mut().right = node7.as_ref().map(Rc::clone);
-        }
-
-        if let Some(node4_) = node4.as_ref() {
-            node4_.borrow_mut().parent = node5.as_ref().map(Rc::clone);
-        }
-
-        if let Some(node5_) = node5.as_ref() {
-            node5_.borrow_mut().parent = node6.as_ref().map(Rc::clone);
-        }
-
-        if let Some(node6_) = node6.as_ref() {
-            node6_.borrow_mut().parent = node7.as_ref().map(Rc::clone);
-        }
-
-        let exp = Some(Rc::new(RefCell::new(Node::new_with(1,
-                                                           node2.as_ref().map(Rc::clone),
-                                                           node3.as_ref().map(Rc::clone),
-                                                           None
-        ))));
-
-        exp
+        assert_eq!(optimal_freelancing(&mut jobs), 13);
     }
 }
