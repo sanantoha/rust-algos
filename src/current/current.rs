c82a94cc742c12819ef6d029d07c073f904c0c96
AntonSakharovAE <anton@alefeducation.com>
2025-04-13 10:04:53 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,46 +1,31 @@
-use crate::graph::EdgeWeightedDigraph;
 
-pub fn shortest_path(graph: EdgeWeightedDigraph, start: usize) -> (Vec<f64>, Vec<Option<usize>>) {
-    (vec![], vec![])
+pub fn min_number_of_jumps(arr: &[i32]) -> i32 {
+    0
 }
 
-pub fn find_negative_weight_circle(graph: EdgeWeightedDigraph, shortest: Vec<f64>, prev: Vec<Option<usize>>) -> Vec<Option<usize>> {
-    vec![]
+pub fn min_number_of_jumps1(arr: &[i32]) -> i32 {
+    0
 }
+
 
 #[cfg(test)]
 mod tests {
-    use std::io::Error;
-    use std::path::PathBuf;
-    use crate::graph::EdgeWeightedDigraph;
-    use super::{find_negative_weight_circle, shortest_path};
+    use super::{min_number_of_jumps, min_number_of_jumps1};
+
+    const ARR: &[i32] = &[3, 4, 2, 1, 2, 3, 7, 1, 1, 1, 3];
 
     #[test]
-    fn test_shortest_path() {
-        if let Ok(graph) = get_graph() {
-            println!("{}", graph);
+    fn test_min_number_of_jumps() {
 
-            let res = shortest_path(graph, 0);
-            println!("{:?}", res);
-            assert_eq!(res, (vec![-9.0, -20.0, -18.0, -2.0, -11.0], vec![Some(4), Some(2), Some(4), Some(0), Some(1)]));
-        }
+        let res = min_number_of_jumps(ARR);
+        println!("{}", res);
+        assert_eq!(res, 4);
     }
 
     #[test]
-    fn test_find_negative_weight_circle() {
-        if let Ok(graph) = get_graph() {
-            println!("{}", graph);
-
-            let shortest = vec![-9.0, -20.0, -18.0, -2.0, -11.0];
-            let prev = vec![Some(4), Some(2), Some(4), Some(0), Some(1)];
-
-            let res = find_negative_weight_circle(graph, shortest, prev);
-            println!("{:?}", res);
-            assert_eq!(res, vec![Some(1), Some(2), Some(4)]);
-        }
-    }
-
-    fn get_graph() -> Result<EdgeWeightedDigraph, Error> {
-        EdgeWeightedDigraph::from_file(PathBuf::from("src/graph/bellmanFord.txt"))
+    fn test_min_number_of_jumps1() {
+        let res = min_number_of_jumps1(ARR);
+        println!("{}", res);
+        assert_eq!(res, 4);
     }
 }
