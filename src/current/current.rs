e0f5884ba6aebe8af4bd2246dc36375a4d4ff03d
AntonSakharovAE <anton@alefeducation.com>
2025-04-15 15:00:40 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,26 +1,82 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+use crate::list::ListNode;
 
-pub fn same_bsts(arr1: &[i32], arr2: &[i32]) -> bool {
+pub fn is_cycle(root: &Option<Rc<RefCell<ListNode>>>) -> bool {
     false
 }
 
-pub fn same_bsts1(arr1: &[i32], arr2: &[i32]) -> bool {
+pub fn is_cycle2(root: &Rc<RefCell<ListNode>>) -> bool {
     false
 }
+
+pub fn is_cycle_ws(root: &Option<Rc<RefCell<ListNode>>>) -> bool {
+    false
+}
+
+pub fn is_cycle_ws2(root: &Rc<RefCell<ListNode>>) -> bool {
+    false
+}
+
 
 #[cfg(test)]
 mod tests {
-    use super::{same_bsts, same_bsts1};
 
-    const ARR_ONE: &[i32] = &[10, 15, 8, 12, 94, 81, 5, 2, 11];
-    const ARR_TWO: &[i32] = &[10, 8, 5, 15, 2, 12, 11, 94, 81];
+    use std::rc::Rc;
+    use std::cell::RefCell;
+    use crate::list::ListNode;
+    use super::{is_cycle, is_cycle2, is_cycle_ws, is_cycle_ws2};
 
     #[test]
-    fn test_same_bsts() {
-        assert!(same_bsts(ARR_ONE, ARR_TWO));
+    pub fn test_is_cycle_if_circle() {
+        let root = Some(create_circle_list());
+
+        assert!(is_cycle(&root));
     }
 
     #[test]
-    fn test_same_bsts1() {
-        assert!(same_bsts1(ARR_ONE, ARR_TWO));
+    pub fn test_is_cycle_if_circle2() {
+        let root = create_circle_list();
+
+        assert!(is_cycle2(&root));
+    }
+
+    #[test]
+    pub fn test_is_cycle_ws_if_circle() {
+        let root = Some(create_circle_list());
+
+        assert!(is_cycle_ws(&root));
+    }
+
+    #[test]
+    pub fn test_is_cycle_ws_if_circle2() {
+        let root = create_circle_list();
+
+        assert!(is_cycle_ws2(&root));
+    }
+
+    #[test]
+    pub fn test_is_cycle_() {
+        let root = Some(create_list());
+        assert!(!is_cycle(&root));
+    }
+
+    #[test]
+    pub fn test_ic_cycle_ws() {
+        let root = Some(create_list());
+        assert!(!is_cycle_ws(&root));
+    }
+
+    pub fn create_circle_list() -> Rc<RefCell<ListNode>> {
+        let node0 = ListNode::with_next(0, Some(ListNode::with_next(1, Some(ListNode::new(3)))));
+        let node4 = ListNode::with_next(4, Some(ListNode::with_next(5, Some(ListNode::with_next(6, Some(ListNode::with_next(7, Some(Rc::clone(&node0)))))))));
+
+        node0.borrow().next.as_ref().unwrap().borrow().next.as_ref().unwrap().borrow_mut().next = Some(Rc::clone(&node4));
+
+        node0
+    }
+
+    pub fn create_list() -> Rc<RefCell<ListNode>> {
+        ListNode::with_next(0, Some(ListNode::with_next(1, Some(ListNode::new(2)))))
     }
 }
