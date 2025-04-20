0e720258989d8c19e194d8d2742b7bb1a064f052
AntonSakharovAE <anton@alefeducation.com>
2025-04-20 12:44:43 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,23 +1,65 @@
 
-pub fn find_ladders<'a>(begin_word: &'a str, end_word: &'a str, word_list: Vec<&'a str>) -> Vec<Vec<&'a str>> {
+pub fn lds(arr: &[i32]) -> i32 {
+    0
+}
 
+pub fn lds1(arr: &[i32]) -> i32 {
+    0
+}
+
+pub fn lds_list(arr: &[i32]) -> Vec<i32> {
     vec![]
 }
 
+pub fn lds_list1(arr: &[i32]) -> Vec<i32> {
+    vec![]
+}
 
 #[cfg(test)]
 mod tests {
-    use super::find_ladders;
+    use super::{lds, lds1, lds_list, lds_list1};
+
+    const ARR0: &[i32] = &[5,6,7,6,5,4,3,10,14,12];
+
+    const ARR1: &[i32] = &[100, 10, 9, 8, 7, 6, 5, 90, 80, 70, 60, 50, 40, 30, 20];
 
     #[test]
-    fn test_find_ladders() {
-        let begin_word = "hit";
-        let end_word = "cog";
-        let word_list = vec!["hot", "dot", "dog", "lot", "log", "cog"];
+    fn test_lds_case0() {
+        assert_eq!(lds(ARR0), 5);
+    }
 
-        let res = find_ladders(begin_word, end_word, word_list);
-        println!("{:?}", res);
+    #[test]
+    fn test_lds_case1() {
+        assert_eq!(lds(ARR1), 9);
+    }
 
-        assert_eq!(res, vec![vec!["hit", "hot", "dot", "dog", "cog"], vec!["hit", "hot", "lot", "log", "cog"]]);
+    #[test]
+    fn test_lds1_case0() {
+        assert_eq!(lds1(ARR0), 5);
+    }
+
+    #[test]
+    fn test_lds1_case1() {
+        assert_eq!(lds1(ARR1), 9);
+    }
+
+    #[test]
+    fn test_lds_list_case0() {
+        assert_eq!(lds_list(ARR0), vec![7, 6, 5, 4, 3]);
+    }
+
+    #[test]
+    fn test_lds_list_case1() {
+        assert_eq!(lds_list(ARR1), vec![100, 90, 80, 70, 60, 50, 40, 30, 20]);
+    }
+
+    #[test]
+    fn test_lds_list1_case0() {
+        assert_eq!(lds_list1(ARR0), vec![7, 6, 5, 4, 3]);
+    }
+
+    #[test]
+    fn test_lds_list1_case1() {
+        assert_eq!(lds_list1(ARR1), vec![100, 90, 80, 70, 60, 50, 40, 30, 20]);
     }
 }
