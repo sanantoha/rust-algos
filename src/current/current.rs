0f388361b7383027c68b0f0dbbf2a96b17cf6edf
AntonSakharovAE <anton@alefeducation.com>
2025-04-22 07:16:41 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,31 +1,48 @@
 
-pub fn powerset(arr: &[i32]) -> Vec<Vec<i32>> {
-    return vec![]
+pub fn all_paths_source_target_rec(graph: &[&[usize]]) -> Vec<Vec<usize>> {
+    vec![]
 }
 
-pub fn powerset_rec(arr: &[i32]) -> Vec<Vec<i32>> {
-    return vec![]
+pub fn all_paths_source_target(graph: &[&[usize]]) -> Vec<Vec<usize>> {
+    vec![]
 }
-
 
 #[cfg(test)]
 mod tests {
-    use super::powerset;
-    use super::powerset_rec;
+    use super::{all_paths_source_target_rec, all_paths_source_target};
+
+    const GRAPH: &[&[usize]] = &[
+        &[1, 2],
+        &[3],
+        &[3],
+        &[],
+    ];
+
+    const GRAPH1: &[&[usize]] = &[
+        &[4, 3, 1],
+        &[3, 2, 4],
+        &[3],
+        &[4],
+        &[],
+    ];
 
     #[test]
-    fn it_powerset() {
-        let res = powerset(&[1, 2, 3]);
-        println!("{:?}", res);
-
-        assert_eq!(res, vec![vec![], vec![1], vec![2], vec![1, 2], vec![3], vec![1, 3], vec![2, 3], vec![1, 2, 3]]);
+    fn test_all_paths_from_source_target_rec() {
+        assert_eq!(all_paths_source_target_rec(GRAPH), vec![vec![0, 1, 3], vec![0, 2, 3]]);
     }
 
     #[test]
-    fn it_powerset_rec() {
-        let res = powerset_rec(&[1, 2, 3]);
-        println!("{:?}", res);
+    fn test_all_paths_from_source_target_rec_case1() {
+        assert_eq!(all_paths_source_target_rec(GRAPH1), vec![vec![0, 4], vec![0, 3, 4], vec![0, 1, 3, 4], vec![0, 1, 2, 3, 4], vec![0, 1, 4]]);
+    }
 
-        assert_eq!(res, vec![vec![], vec![1], vec![2], vec![1, 2], vec![3], vec![1, 3], vec![2, 3], vec![1, 2, 3]]);
+    #[test]
+    fn test_all_paths_from_source_target() {
+        assert_eq!(all_paths_source_target(GRAPH), vec![vec![0, 2, 3], vec![0, 1, 3]]);
+    }
+
+    #[test]
+    fn test_all_paths_from_source_target_case1() {
+        assert_eq!(all_paths_source_target(GRAPH1), vec![vec![0, 1, 4], vec![0, 1, 2, 3, 4], vec![0, 1, 3, 4], vec![0, 3, 4], vec![0, 4]]);
     }
 }
