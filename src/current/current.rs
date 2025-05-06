d45e4afda7fe4617c50f79cf5446eb67f68cd909
AntonSakharovAE <anton@alefeducation.com>
2025-05-06 07:01:32 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,33 +1,154 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+use crate::list::ListNode;
 
-pub fn stable_internships(interns: &[&[usize]], teams: &[&[usize]]) -> Vec<(usize, usize)> {
-    vec![]
+pub fn remove_nth_from_end(head: Option<Rc<RefCell<ListNode>>>, n: i32) -> Option<Rc<RefCell<ListNode>>> {
+    None
 }
 
 
 #[cfg(test)]
 mod tests {
-    use super::stable_internships;
+    use std::rc::Rc;
+    use crate::list::{Displayable, ListNode};
+    use super::remove_nth_from_end;
 
     #[test]
-    fn it_stable_internships() {
+    fn test_remove_nth_from_end_case1() {
+        let head = ListNode::with_next(1,
+                                       Some(ListNode::with_next(2,
+                                                                Some(ListNode::with_next(3,
+                                                                                         Some(ListNode::with_next(4,
+                                                                                                                  Some(ListNode::new(5)))))))));
 
-        let interns: &[&[usize]] = &[
-            &[0, 1, 2],
-            &[0, 2, 1],
-            &[1, 2, 0],
-        ];
 
-        let teams: &[&[usize]] = &[
-            &[2, 1, 0],
-            &[0, 1, 2],
-            &[0, 1, 2]
-        ];
+        let exp_list = ListNode::with_next(1,
+                                           Some(ListNode::with_next(2,
+                                                                    Some(ListNode::with_next(3,
+                                                                                             Some(ListNode::new(4)))))));
 
-        let mut res = stable_internships(interns, teams);
-        res.sort_by_key(|x| x.0);
+        let res = remove_nth_from_end(Some(head), 1);
 
-        println!("{:?}", res);
+        if let Some(display) = Displayable::from_option(res.as_ref().map(Rc::clone)) {
+            println!("{}", display);
+        }
 
-        assert_eq!(res, vec![(0, 1), (1, 0), (2, 2)])
+        assert_eq!(res, Some(exp_list));
+    }
+
+    #[test]
+    fn test_remove_nth_from_end_case2() {
+        let head = ListNode::with_next(1,
+                                       Some(ListNode::with_next(2,
+                                                                Some(ListNode::with_next(3,
+                                                                                         Some(ListNode::with_next(4,
+                                                                                                                  Some(ListNode::new(5)))))))));
+
+
+        let exp_list = ListNode::with_next(1,
+                                           Some(ListNode::with_next(2,
+                                                                    Some(ListNode::with_next(3,
+                                                                                             Some(ListNode::new(5)))))));
+
+        let res = remove_nth_from_end(Some(head), 2);
+
+        if let Some(display) = Displayable::from_option(res.as_ref().map(Rc::clone)) {
+            println!("{}", display);
+        }
+
+        assert_eq!(res, Some(exp_list));
+    }
+
+    #[test]
+    fn test_remove_nth_from_end_case3() {
+        let head = ListNode::with_next(1,
+                                       Some(ListNode::with_next(2,
+                                                                Some(ListNode::with_next(3,
+                                                                                         Some(ListNode::with_next(4,
+                                                                                                                  Some(ListNode::new(5)))))))));
+
+
+        let exp_list = ListNode::with_next(1,
+                                           Some(ListNode::with_next(2,
+                                                                    Some(ListNode::with_next(4,
+                                                                                             Some(ListNode::new(5)))))));
+
+        let res = remove_nth_from_end(Some(head), 3);
+
+        if let Some(display) = Displayable::from_option(res.as_ref().map(Rc::clone)) {
+            println!("{}", display);
+        }
+
+        assert_eq!(res, Some(exp_list));
+    }
+
+    #[test]
+    fn test_remove_nth_from_end_case4() {
+        let head = ListNode::with_next(1,
+                                       Some(ListNode::with_next(2,
+                                                                Some(ListNode::with_next(3,
+                                                                                         Some(ListNode::with_next(4,
+                                                                                                                  Some(ListNode::new(5)))))))));
+
+
+        let exp_list = ListNode::with_next(1,
+                                           Some(ListNode::with_next(3,
+                                                                    Some(ListNode::with_next(4,
+                                                                                             Some(ListNode::new(5)))))));
+
+        let res = remove_nth_from_end(Some(head), 4);
+
+        if let Some(display) = Displayable::from_option(res.as_ref().map(Rc::clone)) {
+            println!("{}", display);
+        }
+
+        assert_eq!(res, Some(exp_list));
+    }
+
+    #[test]
+    fn test_remove_nth_from_end_case5() {
+        let head = ListNode::with_next(1,
+                                       Some(ListNode::with_next(2,
+                                                                Some(ListNode::with_next(3,
+                                                                                         Some(ListNode::with_next(4,
+                                                                                                                  Some(ListNode::new(5)))))))));
+
+
+        let exp_list = ListNode::with_next(2,
+                                           Some(ListNode::with_next(3,
+                                                                    Some(ListNode::with_next(4,
+                                                                                             Some(ListNode::new(5)))))));
+
+        let res = remove_nth_from_end(Some(head), 5);
+
+        if let Some(display) = Displayable::from_option(res.as_ref().map(Rc::clone)) {
+            println!("{}", display);
+        }
+
+        assert_eq!(res, Some(exp_list));
+    }
+
+    #[test]
+    fn test_remove_nth_from_end_case6() {
+        let head = ListNode::with_next(1,
+                                       Some(ListNode::with_next(2,
+                                                                Some(ListNode::with_next(3,
+                                                                                         Some(ListNode::with_next(4,
+                                                                                                                  Some(ListNode::new(5)))))))));
+
+
+        let exp_list = ListNode::with_next(1,
+                                           Some(ListNode::with_next(2,
+                                                                    Some(ListNode::with_next(3,
+                                                                                             Some(ListNode::with_next(4,
+                                                                                                                      Some(ListNode::new(5)))))))));
+
+        let res = remove_nth_from_end(Some(head), 6);
+
+        if let Some(display) = Displayable::from_option(res.as_ref().map(Rc::clone)) {
+            println!("{}", display);
+        }
+
+        assert_eq!(res, Some(exp_list));
     }
 }
