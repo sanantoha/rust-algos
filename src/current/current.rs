b1a774bcf18d5370d824e8ef9914f0787e671afb
AntonSakharovAE <anton@alefeducation.com>
2025-05-09 07:58:07 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,48 +1,23 @@
-use std::collections::HashMap;
-use std::rc::Rc;
-use crate::graph::EdgeT;
 
-pub fn dfs_rec<'a>(graph: &'a HashMap<String, Vec<Rc<EdgeT<String>>>>, start: &'a str) -> Vec<&'a str> {
-    return vec![]
+pub fn longest_common_subsequence(s1: &str, s2: &str) -> Vec<char> {
+    vec![]
 }
 
-pub fn dfs<'a>(graph: &'a HashMap<String, Vec<Rc<EdgeT<String>>>>, start: &'a str) -> Vec<&'a str> {
-    return vec![]
+pub fn longest_common_subsequence1(s1: &str, s2: &str) -> Vec<char> {
+    vec![]
 }
 
 #[cfg(test)]
 mod tests {
-    use std::path::PathBuf;
-
-    use super::dfs_rec;
-    use super::dfs;
-    use crate::graph::{graph_from_file, graph_to_string};
-
-    const PATH: &str = "src/graph/dfs.txt";
-
-    const EXP: &[&str] = &["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"];
+    use super::{longest_common_subsequence, longest_common_subsequence1};
 
     #[test]
-    fn it_dfs_rec() {
-        if let Ok(graph) = graph_from_file(PathBuf::from(PATH)) {
-            let graph_str = graph_to_string(&graph);
-            println!("{}", graph_str);
-
-            let res = dfs_rec(&graph, "0");
-            println!("{:?}", res);
-            assert_eq!(res, EXP)
-        }
+    fn test_longest_common_subsequence() {
+        assert_eq!(longest_common_subsequence("ZXVVYZW", "XKYKZPW"), vec!['X', 'Y', 'Z', 'W']);
     }
 
     #[test]
-    fn it_dfs() {
-        if let Ok(graph) = graph_from_file(PathBuf::from(PATH)) {
-            let graph_str = graph_to_string(&graph);
-            println!("{}", graph_str);
-
-            let res = dfs(&graph, "0");
-            println!("{:?}", res);
-            assert_eq!(res, EXP)
-        }
+    fn test_longest_common_subsequence1() {
+        assert_eq!(longest_common_subsequence1("ZXVVYZW", "XKYKZPW"), vec!['X', 'Y', 'Z', 'W']);
     }
 }
