602e2bbf114eaad0f8e23278dca2ba8ad0b4d0db
AntonSakharovAE <anton@alefeducation.com>
2025-05-13 07:22:47 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,18 +1,36 @@
 
-pub fn first_duplicate_value(arr: &mut [i32]) -> i32 {
-    -1
+pub fn search(arr: &[i32], target: i32) -> Result<usize, usize> {
+    Err(0)
+}
+
+pub fn search1(arr: &[i32], target: i32) -> Result<usize, usize> {
+    Err(0)
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::arrays::first_duplicate_value::first_duplicate_value;
-    use super::first_duplicate_value;
+    use super::{search, search1};
+
+    const ARR: &[i32] = &[40, 50, 60, 70, 80, 90, 0, 10, 20, 30, 31, 32, 33, 34, 35];
+    const TARGET: i32 = 90;
 
     #[test]
-    pub fn test_first_duplicate_value() {
+    fn test_search() {
+        assert_eq!(search(ARR, TARGET), Ok(5));
+    }
 
-        let mut arr = [2,1,3,4,5,6,2,7,8,9];
+    #[test]
+    fn test_search_case1() {
+        assert_eq!(search(ARR, 11), Err(8));
+    }
 
-        assert_eq!(first_duplicate_value(&mut arr), 2);
+    #[test]
+    fn test_search1() {
+        assert_eq!(search1(ARR, TARGET), Ok(5));
+    }
+
+    #[test]
+    fn test_search1_case1() {
+        assert_eq!(search1(ARR, 11), Err(8));
     }
 }
