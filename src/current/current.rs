20ec441cc63e068e622974a0295a3325f3270810
AntonSakharovAE <anton@alefeducation.com>
2025-05-23 06:56:50 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,20 +1,43 @@
+use std::cell::RefCell;
+use std::rc::Rc;
+use crate::list::ListNode;
 
-pub fn sqrt(n: i32) -> i32 {
-    0
+pub fn is_palindrome(root: Option<Rc<RefCell<ListNode>>>) -> bool {
+    false
 }
 
 #[cfg(test)]
 mod tests {
-
-    use super::sqrt;
+    use super::is_palindrome;
+    use crate::list::ListNode;
 
     #[test]
-    fn test_sqrt() {
-        assert_eq!(sqrt(16), 4);
+    fn test_is_palindrome() {
+
+        let head = ListNode::with_next(1,
+                                       Some(ListNode::with_next(2,
+                                                                Some(ListNode::with_next(3,
+                                                                                         Some(ListNode::with_next(3,
+                                                                                                                  Some(ListNode::with_next(2,
+                                                                                                                                           Some(ListNode::new(1)))))))))));
+
+        assert!(is_palindrome(Some(head)));
     }
 
     #[test]
-    fn test_sqrt_case1() {
-        assert_eq!(sqrt(8), 2);
+    fn test_is_palindrome_case1() {
+
+        let head = ListNode::with_next(1,
+                                       Some(ListNode::with_next(2,
+                                                                Some(ListNode::with_next(3,
+                                                                                         Some(ListNode::with_next(2,
+                                                                                                                  Some(ListNode::new(1)))))))));
+
+        assert!(is_palindrome(Some(head)));
+    }
+
+    #[test]
+    fn test_is_palindrome_case2() {
+        assert!(!is_palindrome(None))
     }
 }
