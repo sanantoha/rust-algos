0eee39306f0cefc0b28f619f83db55f4cf888f31
AntonSakharovAE <anton@alefeducation.com>
2025-05-28 08:12:48 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,54 +1,28 @@
-use crate::tree::TreeNode;
+use std::collections::HashMap;
 
-pub fn reconstruct_bst(arr: &[i32]) -> Option<Box<TreeNode>> {
-    None
-}
-
-pub fn reconstruct_bst1(arr: &[i32]) -> Option<Box<TreeNode>> {
-    None
+pub fn optimal_freelancing(jobs: &mut Vec<HashMap<&str, i32>>) -> i32 {
+    return 0
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{reconstruct_bst, reconstruct_bst1};
-    use crate::tree::TreeNode;
 
-    const ARR: &[i32] = &[10, 4, 2, 1, 3, 17, 19, 18];
-
-    #[test]
-    fn test_reconstruct_bst() {
-        let exp_tree = create_exp_tree();
-
-        let res = reconstruct_bst(ARR);
-        assert_eq!(res, exp_tree)
-    }
+    use super::optimal_freelancing;
+    use std::collections::HashMap;
 
     #[test]
-    fn test_reconstruct_bst1() {
-        let exp_tree = create_exp_tree();
+    fn it_optimal_freelancing() {
 
-        let res = reconstruct_bst1(ARR);
-        assert_eq!(res, exp_tree)
-    }
+        let mut jobs: Vec<HashMap<&str, i32>> = vec![
+            HashMap::from([("deadline", 2), ("payment", 2)]),
+            HashMap::from([("deadline", 4), ("payment", 3)]),
+            HashMap::from([("deadline", 5), ("payment", 1)]),
+            HashMap::from([("deadline", 7), ("payment", 2)]),
+            HashMap::from([("deadline", 3), ("payment", 1)]),
+            HashMap::from([("deadline", 3), ("payment", 2)]),
+            HashMap::from([("deadline", 1), ("payment", 3)]),
+        ];
 
-    fn create_exp_tree() -> Option<Box<TreeNode>> {
-        let root = Some(Box::new(TreeNode::new(10,
-                                               Some(Box::new(TreeNode::new(4,
-                                                                           Some(Box::new(TreeNode::new(2,
-                                                                                                       Some(Box::new(TreeNode::leaf(1))),
-                                                                                                       Some(Box::new(TreeNode::leaf(3)))
-                                                                           ))),
-                                                                           None
-                                               ))),
-                                               Some(Box::new(TreeNode::new(17,
-                                                                           None,
-                                                                           Some(Box::new(TreeNode::new(19,
-                                                                                                       Some(Box::new(TreeNode::leaf(18))),
-                                                                                                       None
-                                                                           )))
-                                               ))),
-        )));
-
-        root
+        assert_eq!(optimal_freelancing(&mut jobs), 13);
     }
 }
