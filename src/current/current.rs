831ad8ba7b0760c654ebcc48eeb6c526d8c2a53a
AntonSakharovAE <anton@alefeducation.com>
2025-05-30 07:00:25 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,42 +1,27 @@
 use crate::tree::TreeNode;
 
-pub fn get_minimum_difference(root: &Option<Box<TreeNode>>) -> i32 {
-    0
+pub fn permute(arr: &mut [i32]) -> Vec<Vec<i32>> {
+    vec![]
 }
 
 
 #[cfg(test)]
 mod tests {
-    use super::get_minimum_difference;
-    use crate::tree::TreeNode;
+
+    use super::permute;
 
     #[test]
-    fn test_get_minimum_difference() {
-        let root = Some(Box::new(TreeNode::new(
-            4,
-            Some(Box::new(TreeNode::new(
-                2,
-                Some(Box::new(TreeNode::leaf(1))),
-                Some(Box::new(TreeNode::leaf(3))),
-            ))),
-            Some(Box::new(TreeNode::leaf(6))),
-        )));
+    fn it_permute() {
+        let mut arr = vec![1, 2, 3];
+        let exp_res = vec![
+            vec![1, 2, 3],
+            vec![1, 3, 2],
+            vec![2, 1, 3],
+            vec![2, 3, 1],
+            vec![3, 2, 1],
+            vec![3, 1, 2]
+        ];
 
-        assert_eq!(get_minimum_difference(&root), 1);
-    }
-
-    #[test]
-    fn test_minimum_absolute_difference_case1() {
-        let root = Some(Box::new(TreeNode::new(
-            5,
-            Some(Box::new(TreeNode::leaf(0))),
-            Some(Box::new(TreeNode::new(
-                48,
-                Some(Box::new(TreeNode::leaf(12))),
-                Some(Box::new(TreeNode::leaf(50))),
-            ))),
-        )));
-
-        assert_eq!(get_minimum_difference(&root), 2);
+        assert_eq!(permute(&mut arr), exp_res);
     }
 }
