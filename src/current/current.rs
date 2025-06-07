cc80eab801026492e5cb7867efbb2c947f3f5147
AntonSakharovAE <anton@alefeducation.com>
2025-06-07 11:20:10 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,27 +1,17 @@
 
-pub fn ladder_length(begin_word: &str, end_word: &str, word_list: &[&str]) -> i32 {
-    0
+
+pub fn generate_parentheses(cnt: i32) -> Vec<String> {
+    vec![]
 }
+
 
 #[cfg(test)]
 mod tests {
-
-    use super::ladder_length;
-
-    #[test]
-    pub fn test_ladder_length() {
-
-        assert_eq!(ladder_length("hit", "cog", &["hot","dot","dog","lot","log","cog"]), 5);
-    }
+    use super::generate_parentheses;
 
     #[test]
-    pub fn test_ladder_length_without_end_word() {
-        assert_eq!(ladder_length("hit", "cog", &["hot","dot","dog","lot","log"]), 0);
-    }
+    fn it_generate_parentheses() {
 
-    #[test]
-    pub fn test_ladder_length_mama() {
-
-        assert_eq!(ladder_length("MAMA", "SIRI", &["SAMA", "SIMA", "SIRA", "SIRI", "MISA", "DISA"]), 5);
+        assert_eq!(generate_parentheses(3), vec!["((()))", "(()())", "(())()", "()(())", "()()()"]);
     }
 }
