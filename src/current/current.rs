df96b479b559d878f848cfa540a9cb262b65281f
AntonSakharovAE <anton@alefeducation.com>
2025-06-08 12:49:44 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,17 +1,25 @@
 
-
-pub fn generate_parentheses(cnt: i32) -> Vec<String> {
+pub fn product(arr: &[i32]) -> Vec<i32> {
     vec![]
 }
 
 
 #[cfg(test)]
 mod tests {
-    use super::generate_parentheses;
+    use super::product;
 
     #[test]
-    fn it_generate_parentheses() {
+    fn it_product() {
+        assert_eq!(product(&[1, 2, 3, 4]), vec![24, 12, 8, 6]);
+    }
 
-        assert_eq!(generate_parentheses(3), vec!["((()))", "(()())", "(())()", "()(())", "()()()"]);
+    #[test]
+    fn it_product_if_one_zero() {
+        assert_eq!(product(&[1, 2, 0, 4]), vec![0, 0, 8, 0]);
+    }
+
+    #[test]
+    fn it_product_if_two_zero() {
+        assert_eq!(product(&[0, 2, 0, 4]), vec![0, 0, 0, 0]);
     }
 }
