d21d17fdb4d2a59eb044407ead413b464b067b75
AntonSakharovAE <anton@alefeducation.com>
2025-06-13 11:03:13 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,31 +1,23 @@
-use std::cell::RefCell;
-use std::rc::Rc;
-use crate::list::ListNode;
 
-pub fn add_two_numbers(l1: &Option<Rc<RefCell<ListNode>>>, l2: &Option<Rc<RefCell<ListNode>>>) -> Option<Rc<RefCell<ListNode>>> {
-    None
+
+pub fn reverse(source: &str) -> String {
+    "".to_owned()
 }
+
 
 #[cfg(test)]
 mod tests {
-
-    use crate::list::Displayable;
-    use crate::list::ListNode;
-    use super::add_two_numbers;
+    use super::reverse;
 
     #[test]
-    pub fn test_add_two_numbers() {
+    fn test_reverse() {
+        let exp = "     example   good a ".to_string();
+        assert_eq!(reverse(" a good   example     "), exp);
+    }
 
-
-        let l1 = Some(ListNode::with_next(1, Some(ListNode::with_next(0, Some(ListNode::with_next(9, Some(ListNode::new(9))))))));
-        let l2 = Some(ListNode::with_next(7, Some(ListNode::with_next(3, Some(ListNode::new(2))))));
-
-        let exp = Some(ListNode::with_next(8, Some(ListNode::with_next(3, Some(ListNode::with_next(1, Some(ListNode::with_next(0, Some(ListNode::new(1))))))))));
-
-        let res = add_two_numbers(&l1, &l2);
-        if let Some(disp_list_node) = Displayable::from_option(res.clone()) {
-            println!("{}", disp_list_node);
-        }
-        assert_eq!(res, exp);
+    #[test]
+    fn test_reverse_case1() {
+        let exp = "whitespace   of lot     a has     string      this".to_string();
+        assert_eq!(reverse("this      string     has a     lot of   whitespace"), exp);
     }
 }
