de8539aaf99b5f2475d1427079bf94f3f7e50076
AntonSakharovAE <anton@alefeducation.com>
2025-06-16 09:55:52 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,27 +1,41 @@
 
-pub fn min_swaps_required(src: &str) -> Option<i32> {
-    None
+pub fn reverse_string(arr: &[char]) {
+
 }
 
+pub fn rob(arr: &[i32]) -> i32 {
+    0
+}
+
+pub fn rob1(arr: &[i32]) -> i32 {
+    0
+}
 
 #[cfg(test)]
 mod tests {
-    use super::min_swaps_required;
+    use super::reverse_string;
 
     #[test]
-    fn it_min_swaps_required() {
+    fn it_reverse_string() {
 
-        assert_eq!(min_swaps_required("0100101"), Some(2));
+        let mut arr = ['h', 'e', 't', 'l', 'l', 'o'];
+
+        reverse_string(&mut arr);
+
+        assert_eq!(arr, ['o', 'l', 'l', 't', 'e', 'h']);
+    }
+
+    use super::{rob, rob1};
+
+    #[test]
+    fn it_rob() {
+
+        assert_eq!(rob(&[4,1,2,7,5,3,1]), 14);
     }
 
     #[test]
-    fn it_no_way_to_make_palindrom() {
+    fn it_rob1() {
 
-        assert_eq!(min_swaps_required("1110"), None);
-    }
-
-    #[test]
-    fn it_one_swap() {
-        assert_eq!(min_swaps_required("11101"), Some(1));
+        assert_eq!(rob1(&[4,1,2,7,5,3,1]), 14);
     }
 }
