e8d79641547dd0166e85d722e9ae235350f84de8
AntonSakharovAE <anton@alefeducation.com>
2025-06-17 09:42:37 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,41 +1,32 @@
+use crate::tree::TreeNode;
 
-pub fn reverse_string(arr: &[char]) {
-
-}
-
-pub fn rob(arr: &[i32]) -> i32 {
-    0
-}
-
-pub fn rob1(arr: &[i32]) -> i32 {
-    0
+pub fn level_order(root: Box<TreeNode>) -> Vec<Vec<i32>> {
+    vec![]
 }
 
 #[cfg(test)]
 mod tests {
-    use super::reverse_string;
+    use crate::tree::TreeNode;
+    use super::level_order;
+
 
     #[test]
-    fn it_reverse_string() {
+    fn it_level_order() {
+        let root = Box::new(TreeNode::new(5,
+                                          Some(Box::new(TreeNode::new(2,
+                                                                      Some(Box::new(TreeNode::leaf(1))),
+                                                                      Some(Box::new(TreeNode::leaf(3)))))),
+                                          Some(Box::new(TreeNode::new(10,
+                                                                      Some(Box::new(TreeNode::new(7,
+                                                                                                  Some(Box::new(TreeNode::leaf(6))),
+                                                                                                  Some(Box::new(TreeNode::leaf(8)))))),
+                                                                      Some(Box::new(TreeNode::new(15,
+                                                                                                  Some(Box::new(TreeNode::leaf(14))),
+                                                                                                  Some(Box::new(TreeNode::leaf(17)))))))))),
+        );
 
-        let mut arr = ['h', 'e', 't', 'l', 'l', 'o'];
-
-        reverse_string(&mut arr);
-
-        assert_eq!(arr, ['o', 'l', 'l', 't', 'e', 'h']);
-    }
-
-    use super::{rob, rob1};
-
-    #[test]
-    fn it_rob() {
-
-        assert_eq!(rob(&[4,1,2,7,5,3,1]), 14);
-    }
-
-    #[test]
-    fn it_rob1() {
-
-        assert_eq!(rob1(&[4,1,2,7,5,3,1]), 14);
+        let res = level_order(root);
+        println!("{res:?}");
+        assert_eq!(res, vec![vec![5], vec![2, 10], vec![1, 3, 7, 15], vec![6, 8, 14, 17]]);
     }
 }
