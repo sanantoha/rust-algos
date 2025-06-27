a84076c3d068aeaa497dfe403c4097b3e90e7259
AntonSakharovAE <anton@alefeducation.com>
2025-06-27 06:04:49 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,42 +1,41 @@
-use std::cell::RefCell;
-use std::rc::Rc;
-use crate::list::ListNode;
 
-// O(n * log(v)) time | O(1) space
-fn insert_greatest_common_divisors(head: Option<Rc<RefCell<ListNode>>>) -> Option<Rc<RefCell<ListNode>>> {
-    None
-}
-
-fn gcd(x: i32, y: i32) -> i32 {
-    0
+pub fn string_without_aaa_bbb(a: i32, b: i32) -> String {
+    "".to_owned()
 }
 
 
 #[cfg(test)]
 mod tests {
-    use crate::list::{Displayable, ListNode};
-    use super::{gcd, insert_greatest_common_divisors};
+
+    use super::string_without_aaa_bbb;
 
     #[test]
-    fn test_insert_greatest_common_divisors() {
+    fn test_string_without_aaa_bbb() {
+        assert_eq!(string_without_aaa_bbb(1, 1), "ab");
+    }
 
-        let head = ListNode::with_next(18, Some(ListNode::with_next(6, Some(ListNode::with_next(10, Some(ListNode::new(3)))))));
-        let exp_head = Some(
-            ListNode::with_next(18,
-                                Some(ListNode::with_next(6,
-                                                         Some(ListNode::with_next(6,
-                                                                                  Some(ListNode::with_next(2,
-                                                                                                           Some(ListNode::with_next(10,
-                                                                                                                                    Some(ListNode::with_next(1,
-                                                                                                                                                             Some(ListNode::new(3)))))))))))))
-        );
+    #[test]
+    fn test_string_without_aaa_bbb_case1() {
+        assert_eq!(string_without_aaa_bbb(3, 3), "abbaab");
+    }
 
-        println!("{:?}", gcd(6, 10));
-        let res = insert_greatest_common_divisors(Some(head));
-        // 18 -> 6 -> 6 -> 2 -> 10 -> 1 -> 3 -> null
-        if let Some(disp_list_node) = Displayable::from_option(res.clone()) {
-            println!("{}", disp_list_node);
-        }
-        assert_eq!(res, exp_head);
+    #[test]
+    fn test_string_without_aaa_bbb_case2() {
+        assert_eq!(string_without_aaa_bbb(2, 5), "babbabb");
+    }
+
+    #[test]
+    fn test_string_without_aaa_bbb_case3() {
+        assert_eq!(string_without_aaa_bbb(5, 3), "aabaabab");
+    }
+
+    #[test]
+    fn test_string_without_aaa_bbb_case4() {
+        assert_eq!(string_without_aaa_bbb(3, 3), "abbaab");
+    }
+
+    #[test]
+    fn test_string_without_aaa_bbb_case5() {
+        assert_eq!(string_without_aaa_bbb(1, 4), "bbabb");
     }
 }
