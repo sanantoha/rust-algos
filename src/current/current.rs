df6fbe1d331d98700e4db6e4da5ebc144910c900
AntonSakharovAE <anton@alefeducation.com>
2025-06-30 10:18:54 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,24 +1,55 @@
+use crate::tree::TreeNode;
 
-pub fn min_window(s: &str, t: &str) -> String {
-    "".to_owned()
+pub fn reconstruct_bst(arr: &[i32]) -> Option<Box<TreeNode>> {
+    None
 }
+
+pub fn reconstruct_bst1(arr: &[i32]) -> Option<Box<TreeNode>> {
+    None
+}
+
 
 #[cfg(test)]
 mod tests {
-    use super::min_window;
+    use super::{reconstruct_bst, reconstruct_bst1};
+    use crate::tree::TreeNode;
+
+    const ARR: &[i32] = &[10, 4, 2, 1, 3, 17, 19, 18];
 
     #[test]
-    fn it_min_window() {
-        assert_eq!(min_window("ADOBECODEBANC", "ABC"), "BANC".to_owned());
+    fn test_reconstruct_bst() {
+        let exp_tree = create_exp_tree();
+
+        let res = reconstruct_bst(ARR);
+        assert_eq!(res, exp_tree)
     }
 
     #[test]
-    fn it_min_window_one_letter() {
-        assert_eq!(min_window("a", "a"), "a".to_owned());
+    fn test_reconstruct_bst1() {
+        let exp_tree = create_exp_tree();
+
+        let res = reconstruct_bst1(ARR);
+        assert_eq!(res, exp_tree)
     }
 
-    #[test]
-    fn it_min_window_imp_result() {
-        assert_eq!(min_window("a", "aa"), "".to_owned());
+    fn create_exp_tree() -> Option<Box<TreeNode>> {
+        let root = Some(Box::new(TreeNode::new(10,
+                                               Some(Box::new(TreeNode::new(4,
+                                                                           Some(Box::new(TreeNode::new(2,
+                                                                                                       Some(Box::new(TreeNode::leaf(1))),
+                                                                                                       Some(Box::new(TreeNode::leaf(3)))
+                                                                           ))),
+                                                                           None
+                                               ))),
+                                               Some(Box::new(TreeNode::new(17,
+                                                                           None,
+                                                                           Some(Box::new(TreeNode::new(19,
+                                                                                                       Some(Box::new(TreeNode::leaf(18))),
+                                                                                                       None
+                                                                           )))
+                                               ))),
+        )));
+
+        root
     }
 }
