ddffcace06ecedb5f6a1910d4e3d06ec22e3a094
AntonSakharovAE <anton@alefeducation.com>
2025-07-04 08:18:52 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,39 +1,31 @@
-use crate::tree::TreeNode;
 
-pub fn build_tree(preorder: &[i32], inorder: &[i32]) -> Option<Box<TreeNode>> {
-    None
+pub fn disk_stacking(disks: &[(i32, i32, i32)]) -> Vec<(i32, i32, i32)> {
+    vec![]
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::tree::TreeNode;
-    use super::build_tree;
+    use super::disk_stacking;
 
     #[test]
-    fn test_build_tree() {
-        let preorder: &[i32] = &[3, 9, 20, 15, 7];
-        let inorder: &[i32] = &[9, 3, 15, 20, 7];
+    fn test_disk_stacking() {
+        let disks: &[(i32, i32, i32)] = &[
+            (2, 1, 2),
+            (3, 2, 3),
+            (2, 2, 8),
+            (2, 3, 4),
+            (2, 2, 1),
+            (4, 4, 5),
+        ];
 
-        let tree = build_tree(preorder, inorder);
+        let exp: Vec<(i32, i32, i32)> = vec![
+            (2, 1, 2),
+            (3, 2, 3),
+            (4, 4, 5)
+        ];
 
-        /*
-                     3
-                  /    \
-                9      20
-                     /   \
-                   15     7
-         */
-
-        let exp_tree = Some(
-            Box::new(TreeNode::new(3,
-                                   Some(Box::new(TreeNode::leaf(9))),
-                                   Some(Box::new(TreeNode::new(20,
-                                                               Some(Box::new(TreeNode::leaf(15))),
-                                                               Some(Box::new(TreeNode::leaf(7))),
-                                   ))),
-            ))
-        );
-
-        assert_eq!(tree, exp_tree);
+        let res = disk_stacking(disks);
+        println!("{:?}", res);
+        assert_eq!(res, exp);
     }
 }
