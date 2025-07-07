88ef7d21cf9431953b78fa53ac89e1e30af88a70
AntonSakharovAE <anton@alefeducation.com>
2025-07-07 07:01:55 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,25 +1,17 @@
-use crate::graph::EdgeWeightedDigraph;
 
-pub fn shortest_path(graph: &EdgeWeightedDigraph, start: usize) -> (Vec<f64>, Vec<Option<usize>>) {
-    (vec![], vec![])
+pub fn find_peak(arr: &[i32]) -> Option<usize> {
+    None
 }
 
 #[cfg(test)]
 mod tests {
-    use std::path::PathBuf;
-    use crate::graph::EdgeWeightedDigraph;
-    use super::shortest_path;
+    use super::find_peak;
 
-    // ShortestPath{shortest=[0.0, 5.0, 8.0, 4.0, 7.0], prev=[-1, 3, 1, 0, 3]}
     #[test]
-    fn test_shortest_path() {
-        if let Ok(graph) = EdgeWeightedDigraph::from_file(PathBuf::from("src/graph/dijkstraShortestPath.txt")) {
-            println!("{}", graph);
+    fn test_find_peak() {
+        let res = find_peak(&[1,2,1,3,4,5,6,5,4,3,2,1]);
+        println!("{:?}", res);
 
-            let res = shortest_path(&graph, 0);
-            println!("{:?}", res);
-
-            assert_eq!(res, (vec![0.0, 5.0, 8.0, 4.0, 7.0], vec![None, Some(3), Some(1), Some(0), Some(3)]))
-        }
+        assert_eq!(res, Some(6));
     }
 }
