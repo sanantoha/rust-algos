98c6ec000e716c64aeecbf679b1e3f765a3c8c92
AntonSakharovAE <anton@alefeducation.com>
2025-07-15 08:02:39 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,27 +1,49 @@
 
-pub fn river_sizes(matrix: &[&[i32]]) -> Vec<i32> {
+pub fn all_paths_source_target_rec(graph: &[&[usize]]) -> Vec<Vec<usize>> {
+    vec![]
+}
+
+pub fn all_paths_source_target(graph: &[&[usize]]) -> Vec<Vec<usize>> {
     vec![]
 }
 
 
 #[cfg(test)]
 mod tests {
-    use super::river_sizes;
+    use super::{all_paths_source_target_rec, all_paths_source_target};
+
+    const GRAPH: &[&[usize]] = &[
+        &[1, 2],
+        &[3],
+        &[3],
+        &[],
+    ];
+
+    const GRAPH1: &[&[usize]] = &[
+        &[4, 3, 1],
+        &[3, 2, 4],
+        &[3],
+        &[4],
+        &[],
+    ];
 
     #[test]
-    pub fn test_river_sizes() {
+    fn test_all_paths_from_source_target_rec() {
+        assert_eq!(all_paths_source_target_rec(GRAPH), vec![vec![0, 1, 3], vec![0, 2, 3]]);
+    }
 
-        let matrix: &[&[i32]] = &[
-            &[1, 0, 0, 1, 0],
-            &[1, 0, 1, 0, 1],
-            &[0, 0, 1, 0, 1],
-            &[1, 0, 1, 1, 0],
-            &[1, 0, 1, 0, 0],
-        ];
+    #[test]
+    fn test_all_paths_from_source_target_rec_case1() {
+        assert_eq!(all_paths_source_target_rec(GRAPH1), vec![vec![0, 4], vec![0, 3, 4], vec![0, 1, 3, 4], vec![0, 1, 2, 3, 4], vec![0, 1, 4]]);
+    }
 
-        let res = river_sizes(matrix);
-        println!("{:?}", res);
+    #[test]
+    fn test_all_paths_from_source_target() {
+        assert_eq!(all_paths_source_target(GRAPH), vec![vec![0, 2, 3], vec![0, 1, 3]]);
+    }
 
-        assert_eq!(res, vec![2, 1, 5, 2, 2])
+    #[test]
+    fn test_all_paths_from_source_target_case1() {
+        assert_eq!(all_paths_source_target(GRAPH1), vec![vec![0, 1, 4], vec![0, 1, 2, 3, 4], vec![0, 1, 3, 4], vec![0, 3, 4], vec![0, 4]]);
     }
 }
