d52b4a0e350f6a584890e4c9ff2304535c09ff30
AntonSakharovAE <anton@alefeducation.com>
2025-07-18 08:45:19 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,64 +1,91 @@
-use crate::tree::TreeNode;
+use std::collections::HashMap;
+use std::rc::Rc;
+use crate::graph::EdgeT;
 
-pub fn lower_common_ancestor<'a>(root: &'a Option<Box<TreeNode>>,
-                                 p: &'a Option<Box<TreeNode>>,
-                                 q: &'a Option<Box<TreeNode>>) -> &'a Option<Box<TreeNode>> {
-    &None
+pub fn mst(graph: &HashMap<String, Vec<Rc<EdgeT<String>>>>) -> HashMap<String, Vec<Rc<EdgeT<String>>>> {
+    HashMap::new()
+}
+
+pub fn mst1(graph: &HashMap<String, Vec<Rc<EdgeT<String>>>>) -> HashMap<String, Vec<Rc<EdgeT<String>>>> {
+    HashMap::new()
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::tree::TreeNode;
-    use super::lower_common_ancestor;
+    use super::{mst, mst1};
+    use crate::graph::{compare_as_map, create_graph, create_graph1, exp_graph, exp_graph1, graph_to_string};
 
+    /*
+        6 5
+        0: 0-1 7.00000
+        1: 1-2 3.00000  0-1 7.00000
+        2: 1-2 3.00000  2-4 3.00000
+        3: 3-4 2.00000
+        4: 3-4 2.00000  4-5 2.00000  2-4 3.00000
+        5: 4-5 2.00000
+    */
     #[test]
-    fn test_lower_common_ancestor() {
-        let root = Some(Box::new(TreeNode::new(
-            6,
-            Some(Box::new(TreeNode::new(
-                2,
-                Some(Box::new(TreeNode::leaf(0))),
-                Some(Box::new(TreeNode::new(
-                    4,
-                    Some(Box::new(TreeNode::leaf(3))),
-                    Some(Box::new(TreeNode::leaf(5))),
-                ))),
-            ))),
-            Some(Box::new(TreeNode::new(
-                8,
-                Some(Box::new(TreeNode::leaf(7))),
-                Some(Box::new(TreeNode::leaf(9))),
-            ))),
-        )));
+    fn test_mst() {
+        let graph = create_graph();
+        let graph_str = graph_to_string(&graph);
+        println!("{}", graph_str);
 
-        let p = Some(Box::new(TreeNode::leaf(0)));
-        let q = Some(Box::new(TreeNode::leaf(5)));
+        let exp_graph = exp_graph();
 
-        let exp_tree = Some(Box::new(TreeNode::new(
-            2,
-            Some(Box::new(TreeNode::leaf(0))),
-            Some(Box::new(TreeNode::new(
-                4,
-                Some(Box::new(TreeNode::leaf(3))),
-                Some(Box::new(TreeNode::leaf(5))),
-            ))),
-        )));
+        let res = mst(&graph);
+        println!("{}", graph_to_string(&exp_graph));
+        println!("{}", graph_to_string(&res));
 
-        let res = lower_common_ancestor(&root, &p, &q);
-        println!("{:?}", res);
-
-        assert_eq!(res, &exp_tree);
+        // assert_eq!(res, exp_graph)
+        assert!(compare_as_map(&res, &exp_graph));
     }
 
     #[test]
-    fn test_lower_common_ancestor_case1() {
-        let root = Some(Box::new(TreeNode::new(2, Some(Box::new(TreeNode::leaf(1))), None)));
-        let p = Some(Box::new(TreeNode::leaf(2)));
-        let q = Some(Box::new(TreeNode::leaf(1)));
+    fn test_mst_case1() {
+        let graph = create_graph1();
+        let graph_str = graph_to_string(&graph);
+        println!("{}", graph_str);
 
-        let res = lower_common_ancestor(&root, &p, &q);
-        println!("{:?}", res);
+        let exp_graph = exp_graph1();
 
-        assert_eq!(res, &root);
+        let res = mst(&graph);
+        println!("{}", graph_to_string(&exp_graph));
+        println!("{}", graph_to_string(&res));
+
+        // assert_eq!(res, exp_graph)
+        assert!(compare_as_map(&res, &exp_graph));
     }
+
+    #[test]
+    fn test_mst1() {
+        let graph = create_graph();
+        let graph_str = graph_to_string(&graph);
+        println!("{}", graph_str);
+
+        let exp_graph = exp_graph();
+
+        let res = mst1(&graph);
+        println!("{}", graph_to_string(&exp_graph));
+        println!("{}", graph_to_string(&res));
+
+        // assert_eq!(res, exp_graph)
+        assert!(compare_as_map(&res, &exp_graph));
+    }
+
+    #[test]
+    fn test_mst1_case1() {
+        let graph = create_graph1();
+        let graph_str = graph_to_string(&graph);
+        println!("{}", graph_str);
+
+        let exp_graph = exp_graph1();
+
+        let res = mst1(&graph);
+        println!("{}", graph_to_string(&exp_graph));
+        println!("{}", graph_to_string(&res));
+
+        // assert_eq!(res, exp_graph)
+        assert!(compare_as_map(&res, &exp_graph));
+    }
+
 }
