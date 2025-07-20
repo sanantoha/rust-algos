66e42df268d89cbfefb2d3cc625a0388e7e8fd38
AntonSakharovAE <anton@alefeducation.com>
2025-07-20 11:12:07 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,129 +1,39 @@
-use crate::tree::TreeNode;
 
-pub fn is_same_tree(t1: &Option<Box<TreeNode>>, t2: &Option<Box<TreeNode>>) -> bool {
-    false
-}
-
-pub fn is_same_tree_iter(t1: &Option<Box<TreeNode>>, t2: &Option<Box<TreeNode>>) -> bool {
-    false
+pub fn gcd(mut a: i32, mut b: i32) -> i32 {
+    return -1
 }
 
 
 #[cfg(test)]
 mod tests {
-    use crate::tree::TreeNode;
-    use super::{is_same_tree, is_same_tree_iter};
+    use super::gcd;
+
 
     #[test]
-    fn test_is_same_tree() {
-        let t1 = create_tree1();
-        let t2 = create_tree1();
-        assert!(is_same_tree(&t1, &t2));
-    }
-
-    #[test]
-    fn test_is_same_tree_iter() {
-        let t1 = create_tree1();
-        let t2 = create_tree1();
-        assert!(is_same_tree_iter(&t1, &t2));
+    fn test_gcd() {
+        let res = gcd(18, 6);
+        println!("{res}");
+        assert_eq!(res, 6);
     }
 
     #[test]
-    fn test_is_same_tree_neg() {
-        let t1 = create_tree1();
-        let t2 = create_tree2();
-        assert!(!is_same_tree(&t1, &t2));
+    fn test_gcd1() {
+        let res = gcd(18, 10);
+        println!("{res}");
+        assert_eq!(res, 2);
     }
 
     #[test]
-    fn test_is_same_tree_iter_neg() {
-        let t1 = create_tree1();
-        let t2 = create_tree2();
-        assert!(!is_same_tree_iter(&t1, &t2));
+    fn test_gcd2() {
+        let res = gcd(17, 11);
+        println!("{res}");
+        assert_eq!(res, 1);
     }
 
     #[test]
-    fn test_is_same_tree_case1() {
-        let t1 = create_tree3();
-        let t2 = create_tree31();
-
-        assert!(!is_same_tree(&t1, &t2));
-    }
-
-    #[test]
-    fn test_is_same_tree_iter_case1() {
-        let t1 = create_tree3();
-        let t2 = create_tree31();
-        assert!(!is_same_tree_iter(&t1, &t2));
-    }
-
-    fn create_tree1() -> Option<Box<TreeNode>> {
-        let tree = TreeNode::new(
-            5,
-            Some(Box::new(TreeNode::new(
-                2,
-                Some(Box::new(TreeNode::leaf(1))),
-                Some(Box::new(TreeNode::leaf(3))),
-            ))),
-            Some(Box::new(TreeNode::new(
-                10,
-                Some(Box::new(TreeNode::leaf(7))),
-                Some(Box::new(TreeNode::leaf(15))),
-            ))),
-        );
-
-        Some(Box::new(tree))
-    }
-
-    fn create_tree2() -> Option<Box<TreeNode>> {
-        let tree = TreeNode::new(
-            5,
-            Some(Box::new(TreeNode::new(
-                2,
-                Some(Box::new(TreeNode::leaf(1))),
-                Some(Box::new(TreeNode::leaf(444444444))),
-            ))),
-            Some(Box::new(TreeNode::new(
-                10,
-                Some(Box::new(TreeNode::leaf(7))),
-                Some(Box::new(TreeNode::leaf(15))),
-            ))),
-        );
-
-        Some(Box::new(tree))
-    }
-
-    fn create_tree3() -> Option<Box<TreeNode>> {
-        let root = TreeNode::new(
-            1,
-            Some(Box::new(TreeNode::new(
-                2,
-                None,
-                Some(Box::new(TreeNode::leaf(3))),
-            ))),
-            Some(Box::new(TreeNode::new(
-                2,
-                None,
-                Some(Box::new(TreeNode::leaf(3))),
-            ))),
-        );
-        Some(Box::new(root))
-    }
-
-    fn create_tree31() -> Option<Box<TreeNode>> {
-        let root = TreeNode::new(
-            1,
-            Some(Box::new(TreeNode::new(
-                2,
-                Some(Box::new(TreeNode::leaf(3))),
-                None,
-            ))),
-            Some(Box::new(TreeNode::new(
-                2,
-                None,
-                Some(Box::new(TreeNode::leaf(3))),
-            ))),
-        );
-        Some(Box::new(root))
+    fn test_gcd3() {
+        let res = gcd(5, 15);
+        println!("{res}");
+        assert_eq!(res, 5);
     }
 }
