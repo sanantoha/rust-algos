b320436487b7420422ef208c27949a5afbb8cf06
AntonSakharovAE <anton@alefeducation.com>
2025-07-23 07:31:05 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,25 +1,79 @@
-use crate::graph::EdgeWeightedDigraph;
+use std::cell::RefCell;
+use std::rc::Rc;
+use crate::graph::Node;
 
-pub fn bfs(graph: &EdgeWeightedDigraph, start: usize) -> Vec<usize> {
-    vec![]
+pub fn clone_graph(node: Rc<RefCell<Node>>) -> Option<Rc<RefCell<Node>>> {
+    None
 }
-
 
 #[cfg(test)]
 mod tests {
-    use std::path::PathBuf;
-    use super::bfs;
-    use crate::graph::EdgeWeightedDigraph;
-
+    use std::cell::RefCell;
+    use std::rc::Rc;
+    use super::clone_graph;
+    use crate::graph::Node;
 
     #[test]
-    fn it_bfs() {
-        if let Ok(graph) = EdgeWeightedDigraph::from_file(PathBuf::from("src/graph/bfs.txt")) {
-            println!("{}", graph);
+    fn test_clone_graph() {
+        let node1 = Rc::new(RefCell::new(Node::new(1)));
+        let node2 = Rc::new(RefCell::new(Node::new(2)));
+        let node3 = Rc::new(RefCell::new(Node::new(3)));
+        let node4 = Rc::new(RefCell::new(Node::new(4)));
 
-            let res = bfs(&graph, 0);
-            println!("{:?}", res);
-            assert_eq!(res, vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]);
+        node1.borrow_mut().neighbors.push(Rc::clone(&node2));
+        node1.borrow_mut().neighbors.push(Rc::clone(&node4));
+
+        node2.borrow_mut().neighbors.push(Rc::clone(&node1));
+        node2.borrow_mut().neighbors.push(Rc::clone(&node3));
+
+        node3.borrow_mut().neighbors.push(Rc::clone(&node2));
+        node3.borrow_mut().neighbors.push(Rc::clone(&node4));
+
+        node4.borrow_mut().neighbors.push(Rc::clone(&node1));
+        node4.borrow_mut().neighbors.push(Rc::clone(&node3));
+
+        let cloned_graph = clone_graph(Rc::clone(&node1));
+
+        match &cloned_graph {
+            Some(copy_node1) => {
+                assert_eq!(copy_node1.borrow().val, 1);
+                assert!(!Rc::ptr_eq(copy_node1, &node1));
+                assert_eq!(copy_node1.borrow().neighbors.len(), 2);
+
+                let copy_node2 = Rc::clone(copy_node1.borrow().neighbors.get(0).unwrap());
+                let copy_node4 = Rc::clone(copy_node1.borrow().neighbors.get(1).unwrap());
+
+                assert_eq!(copy_node2.borrow().val, 2);
+                assert!(!Rc::ptr_eq(&copy_node2, &node2));
+                assert_eq!(copy_node2.borrow().neighbors.len(), 2);
+
+                assert_eq!(copy_node4.borrow().val, 4);
+                assert!(!Rc::ptr_eq(&copy_node4, &node4));
+                assert_eq!(copy_node4.borrow().neighbors.len(), 2);
+
+                let copy_node11 = Rc::clone(copy_node2.borrow().neighbors.get(0).unwrap());
+                let copy_node3 = Rc::clone(copy_node2.borrow().neighbors.get(1).unwrap());
+
+                assert_eq!(copy_node11.borrow().val, 1);
+                assert!(!Rc::ptr_eq(&copy_node11, &node1));
+                assert!(Rc::ptr_eq(&copy_node11, &copy_node1));
+                assert_eq!(copy_node11.borrow().neighbors.len(), 2);
+
+                assert_eq!(copy_node3.borrow().val, 3);
+                assert!(!Rc::ptr_eq(&copy_node3, &node3));
+                assert_eq!(copy_node3.borrow().neighbors.len(), 2);
+
+                let copy_node22 = Rc::clone(copy_node3.borrow().neighbors.get(0).unwrap());
+                let copy_node44 = Rc::clone(copy_node3.borrow().neighbors.get(1).unwrap());
+                assert!(Rc::ptr_eq(&copy_node22, &copy_node2));
+                assert!(Rc::ptr_eq(&copy_node44, &copy_node4));
+
+                let copy_node33 = Rc::clone(copy_node4.borrow().neighbors.get(1).unwrap());
+                assert!(Rc::ptr_eq(&copy_node33, &copy_node3));
+            }
+            None => {
+                assert!(false);
+            }
         }
     }
 }
