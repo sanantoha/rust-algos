89d9712ddf4c0910053d6e98644fdccdd728a747
AntonSakharovAE <anton@alefeducation.com>
2025-07-24 06:46:23 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,79 +1,55 @@
-use std::cell::RefCell;
-use std::rc::Rc;
-use crate::graph::Node;
+use crate::tree::TreeNode;
 
-pub fn clone_graph(node: Rc<RefCell<Node>>) -> Option<Rc<RefCell<Node>>> {
+pub fn find_closest_value_in_bst_rec(root: &Option<Box<TreeNode>>, target: i32) -> Option<i32> {
+    None
+}
+
+pub fn find_closest_value_in_bst(root: &Option<Box<TreeNode>>, target: i32) -> Option<i32> {
     None
 }
 
 #[cfg(test)]
 mod tests {
-    use std::cell::RefCell;
-    use std::rc::Rc;
-    use super::clone_graph;
-    use crate::graph::Node;
+    use crate::tree::TreeNode;
+    use super::{find_closest_value_in_bst_rec, find_closest_value_in_bst};
 
     #[test]
-    fn test_clone_graph() {
-        let node1 = Rc::new(RefCell::new(Node::new(1)));
-        let node2 = Rc::new(RefCell::new(Node::new(2)));
-        let node3 = Rc::new(RefCell::new(Node::new(3)));
-        let node4 = Rc::new(RefCell::new(Node::new(4)));
+    fn test_find_closest_value_in_bst_rec() {
+        let root = create_tree();
 
-        node1.borrow_mut().neighbors.push(Rc::clone(&node2));
-        node1.borrow_mut().neighbors.push(Rc::clone(&node4));
+        assert_eq!(find_closest_value_in_bst_rec(&root, 12), Some(13));
+    }
 
-        node2.borrow_mut().neighbors.push(Rc::clone(&node1));
-        node2.borrow_mut().neighbors.push(Rc::clone(&node3));
+    #[test]
+    fn test_find_closest_value_in_bst() {
+        let root = create_tree();
 
-        node3.borrow_mut().neighbors.push(Rc::clone(&node2));
-        node3.borrow_mut().neighbors.push(Rc::clone(&node4));
+        assert_eq!(find_closest_value_in_bst(&root, 12), Some(13));
+    }
 
-        node4.borrow_mut().neighbors.push(Rc::clone(&node1));
-        node4.borrow_mut().neighbors.push(Rc::clone(&node3));
+    fn create_tree() -> Option<Box<TreeNode>> {
+        let root = TreeNode::new(
+            10,
+            Some(Box::new(TreeNode::new(
+                5,
+                Some(Box::new(TreeNode::new(
+                    2,
+                    Some(Box::new(TreeNode::leaf(1))),
+                    None,
+                ))),
+                Some(Box::new(TreeNode::leaf(5))),
+            ))),
+            Some(Box::new(TreeNode::new(
+                15,
+                Some(Box::new(TreeNode::new(
+                    13,
+                    None,
+                    Some(Box::new(TreeNode::leaf(14))),
+                ))),
+                Some(Box::new(TreeNode::leaf(22))),
+            ))),
+        );
 
-        let cloned_graph = clone_graph(Rc::clone(&node1));
-
-        match &cloned_graph {
-            Some(copy_node1) => {
-                assert_eq!(copy_node1.borrow().val, 1);
-                assert!(!Rc::ptr_eq(copy_node1, &node1));
-                assert_eq!(copy_node1.borrow().neighbors.len(), 2);
-
-                let copy_node2 = Rc::clone(copy_node1.borrow().neighbors.get(0).unwrap());
-                let copy_node4 = Rc::clone(copy_node1.borrow().neighbors.get(1).unwrap());
-
-                assert_eq!(copy_node2.borrow().val, 2);
-                assert!(!Rc::ptr_eq(&copy_node2, &node2));
-                assert_eq!(copy_node2.borrow().neighbors.len(), 2);
-
-                assert_eq!(copy_node4.borrow().val, 4);
-                assert!(!Rc::ptr_eq(&copy_node4, &node4));
-                assert_eq!(copy_node4.borrow().neighbors.len(), 2);
-
-                let copy_node11 = Rc::clone(copy_node2.borrow().neighbors.get(0).unwrap());
-                let copy_node3 = Rc::clone(copy_node2.borrow().neighbors.get(1).unwrap());
-
-                assert_eq!(copy_node11.borrow().val, 1);
-                assert!(!Rc::ptr_eq(&copy_node11, &node1));
-                assert!(Rc::ptr_eq(&copy_node11, &copy_node1));
-                assert_eq!(copy_node11.borrow().neighbors.len(), 2);
-
-                assert_eq!(copy_node3.borrow().val, 3);
-                assert!(!Rc::ptr_eq(&copy_node3, &node3));
-                assert_eq!(copy_node3.borrow().neighbors.len(), 2);
-
-                let copy_node22 = Rc::clone(copy_node3.borrow().neighbors.get(0).unwrap());
-                let copy_node44 = Rc::clone(copy_node3.borrow().neighbors.get(1).unwrap());
-                assert!(Rc::ptr_eq(&copy_node22, &copy_node2));
-                assert!(Rc::ptr_eq(&copy_node44, &copy_node4));
-
-                let copy_node33 = Rc::clone(copy_node4.borrow().neighbors.get(1).unwrap());
-                assert!(Rc::ptr_eq(&copy_node33, &copy_node3));
-            }
-            None => {
-                assert!(false);
-            }
-        }
+        Some(Box::new(root))
     }
 }
