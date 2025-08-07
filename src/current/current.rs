041582b2f3fce843a8c705622c9becffc759535c
AntonSakharovAE <anton@alefeducation.com>
2025-08-07 06:54:43 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,77 +1,26 @@
 
-pub fn rotate(arr: &mut [i32], k: i32) {
-
-}
-
-pub fn rotate1(arr: &mut [i32], k: i32) {
-
+pub fn one_edit(s1: &str, s2: &str) -> bool {
+    false
 }
 
 
 #[cfg(test)]
 mod tests {
-    use super::rotate;
-    use super::rotate1;
+    use super::one_edit;
 
     #[test]
-    fn it_rotate_arr() {
-        let mut arr = [1, 2, 3, 4, 5, 6, 7];
+    fn test_one_edit() {
+        let s1 = String::from("hello");
+        let s2 = String::from("helo");
 
-        rotate(&mut arr, 3);
-
-        println!("{arr:?}");
-        assert_eq!(arr, [5, 6, 7, 1, 2, 3, 4])
+        assert!(one_edit(&s1, &s2));
     }
 
     #[test]
-    fn it_rotate_arr1() {
-        let mut arr = [-1,-100,3,99];
+    fn test_one_edit_case1() {
+        let s1 = String::from("hello");
+        let s2 = String::from("hdlo");
 
-        rotate(&mut arr, 2);
-
-        println!("{arr:?}");
-        assert_eq!(arr, [3, 99, -1, -100])
+        assert!(!one_edit(&s1, &s2));
     }
-
-    #[test]
-    fn it_rotate_arr2() {
-        let mut arr = [1, 2, 3];
-
-        rotate(&mut arr, 2);
-
-        println!("{arr:?}");
-        assert_eq!(arr, [2, 3, 1])
-    }
-
-    #[test]
-    fn it_rotate1_arr() {
-        let mut arr = [1, 2, 3, 4, 5, 6, 7];
-
-        rotate1(&mut arr, 3);
-
-        println!("{arr:?}");
-        assert_eq!(arr, [5, 6, 7, 1, 2, 3, 4])
-    }
-
-    #[test]
-    fn it_rotate1_arr1() {
-        let mut arr = [-1,-100,3,99];
-
-        rotate1(&mut arr, 2);
-
-        println!("{arr:?}");
-        assert_eq!(arr, [3, 99, -1, -100])
-    }
-
-    #[test]
-    fn it_rotate1_arr2() {
-        let mut arr = [1, 2, 3];
-
-        rotate1(&mut arr, 2);
-
-        println!("{arr:?}");
-        assert_eq!(arr, [2, 3, 1])
-    }
-
-
 }
