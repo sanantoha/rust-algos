ff435c5bb83b439141f2ab3126045f38721f400f
AntonSakharovAE <anton@alefeducation.com>
2025-09-16 22:10:28 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,50 +1,35 @@
 use crate::tree::TreeNode;
 
-pub fn find_node_distance_k(root: &Option<Box<TreeNode>>, target: i32, k: i32) -> Vec<i32> {
-    vec![]
-}
-
-pub fn find_node_distance_k_rec(root: &Option<Box<TreeNode>>, target: i32, k: i32) -> Vec<i32> {
+pub fn bfs(root: &Option<Box<TreeNode>>) -> Vec<i32> {
     vec![]
 }
 
 #[cfg(test)]
 mod tests {
+    use super::bfs;
     use crate::tree::TreeNode;
-    use super::*;
 
     #[test]
-    fn test_find_node_distance_k() {
-        let root = create_tree();
+    pub fn test_bfs() {
 
-        let mut res = find_node_distance_k(&root, 3, 2);
-        res.sort();
-        assert_eq!(res, vec![2, 7, 8]);
-    }
-
-    #[test]
-    fn test_find_node_distance_k_rec() {
-        let root = create_tree();
-
-        let mut res = find_node_distance_k_rec(&root, 3, 2);
-        res.sort();
-        assert_eq!(res, vec![2, 7, 8]);
-    }
-
-    fn create_tree() -> Option<Box<TreeNode>> {
-        let root = Some(Box::new(TreeNode::new(1,
-                                               Some(Box::new(TreeNode::new(2,
-                                                                           Some(Box::new(TreeNode::leaf(4))),
-                                                                           Some(Box::new(TreeNode::leaf(5)))
-                                               ))),
-                                               Some(Box::new(TreeNode::new(3,
-                                                                           None,
-                                                                           Some(Box::new(TreeNode::new(6,
-                                                                                                       Some(Box::new(TreeNode::leaf(7))),
-                                                                                                       Some(Box::new(TreeNode::leaf(8)))
-                                                                           )))
-                                               )))
+        let root = Some(Box::new(TreeNode::new(
+            5,
+            Some(Box::new(TreeNode::new(
+                2,
+                Some(Box::new(TreeNode::leaf(1))),
+                Some(Box::new(TreeNode::leaf(3))),
+            ))),
+            Some(Box::new(TreeNode::new(
+                10,
+                Some(Box::new(TreeNode::leaf(7))),
+                Some(Box::new(TreeNode::new(
+                    15,
+                    Some(Box::new(TreeNode::leaf(13))),
+                    Some(Box::new(TreeNode::leaf(17))),
+                ))),
+            ))),
         )));
-        root
+
+        assert_eq!(bfs(&root), vec![5, 2, 10, 1, 3, 7, 15, 13, 17]);
     }
 }
