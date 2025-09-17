925982cb9f037221858dfb0d1d64ebb51ecd24b3
AntonSakharovAE <anton@alefeducation.com>
2025-09-17 14:09:48 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,20 +1,72 @@
+use std::collections::HashMap;
 
-pub fn subarray_sum(arr: &[i32], k: i32) -> i32 {
-    0
+pub fn sort_rec(graph: &HashMap<String, Vec<String>>) -> Result<Vec<&str>, String> {
+    Err("not found".to_owned())
 }
+
+pub fn sort_iter(graph: &HashMap<String, Vec<String>>) -> Result<Vec<&str>, String> {
+    Err("not found".to_owned())
+}
+
 
 #[cfg(test)]
 mod tests {
-    use crate::arrays::subarray_sum::subarray_sum;
-    use super::subarray_sum;
+    use std::collections::HashMap;
+    use super::{sort_rec, sort_iter};
 
     #[test]
-    fn it_subarray_sum() {
-        assert_eq!(subarray_sum(&[1, 1, 1], 2), 2);
+    fn test_sort_rec() {
+        let mut graph = HashMap::new();
+        graph.insert("A".to_string(), vec!["B".to_string(), "C".to_string(), "D".to_string()]);
+        graph.insert("B".to_string(), vec!["C".to_string()]);
+        graph.insert("C".to_string(), vec!["D".to_string()]);
+        graph.insert("D".to_string(), vec![]);
+
+        let res = sort_rec(&graph);
+
+        assert_eq!(res, Ok(vec!["A", "B", "C", "D"]));
     }
 
     #[test]
-    fn it_subarray_sum_case2() {
-        assert_eq!(subarray_sum(&[1, 2, 3], 3), 2);
+    fn test_sort_rec_case1() {
+        let mut graph = HashMap::new();
+        graph.insert("A".to_string(), vec!["B".to_string(), "C".to_string(), "D".to_string()]);
+        graph.insert("B".to_string(), vec!["C".to_string()]);
+        graph.insert("C".to_string(), vec!["D".to_string()]);
+        graph.insert("D".to_string(), vec!["A".to_string()]);
+
+        let res = sort_rec(&graph);
+
+        println!("{:?}", res);
+        assert!(res.is_err());
+        if let Err(msg) = res {
+            assert!(msg.contains("circle in the graph "));
+        }
+    }
+
+    #[test]
+    fn test_sort_iter() {
+        let mut graph = HashMap::new();
+        graph.insert("A".to_string(), vec!["B".to_string(), "C".to_string(), "D".to_string()]);
+        graph.insert("B".to_string(), vec!["C".to_string()]);
+        graph.insert("C".to_string(), vec!["D".to_string()]);
+        graph.insert("D".to_string(), vec![]);
+
+        let res = sort_iter(&graph);
+
+        assert_eq!(res, Ok(vec!["A", "B", "C", "D"]));
+    }
+
+    #[test]
+    fn test_sort_iter_case1() {
+        let mut graph = HashMap::new();
+        graph.insert("A".to_string(), vec!["B".to_string(), "C".to_string(), "D".to_string()]);
+        graph.insert("B".to_string(), vec!["C".to_string()]);
+        graph.insert("C".to_string(), vec!["D".to_string()]);
+        graph.insert("D".to_string(), vec!["B".to_string()]);
+
+        let res = sort_iter(&graph);
+
+        assert_eq!(res, Err("circle in the graph".to_string()));
     }
 }
