e8f69a9dfce68d7f6db281cee456cc6fe0d2a03b
AntonSakharovAE <anton@alefeducation.com>
2025-09-17 10:36:11 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,16 +1,21 @@
 
-
-pub fn permute(src: &str) -> Vec<String> {
+pub fn find_ladders<'a>(begin_word: &'a str, end_word: &'a str, word_list: Vec<&'a str>) -> Vec<Vec<&'a str>> {
     vec![]
 }
 
-
 #[cfg(test)]
 mod tests {
-    use super::permute;
+    use super::find_ladders;
 
     #[test]
-    fn test_string_permutation() {
-        assert_eq!(permute("abc"), vec!["abc", "acb", "bac", "bca", "cab", "cba"]);
+    fn test_find_ladders() {
+        let begin_word = "hit";
+        let end_word = "cog";
+        let word_list = vec!["hot", "dot", "dog", "lot", "log", "cog"];
+
+        let res = find_ladders(begin_word, end_word, word_list);
+        println!("{:?}", res);
+
+        assert_eq!(res, vec![vec!["hit", "hot", "dot", "dog", "cog"], vec!["hit", "hot", "lot", "log", "cog"]]);
     }
 }
