f3fe4c76ee78771aea1e06fdc6ccec7e208026bb
AntonSakharovAE <anton@alefeducation.com>
2025-09-19 10:15:41 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,35 +1,53 @@
 
-pub fn climb_stairs_dp(n: usize) -> usize {
-    0
+pub fn pow(x: f64, n: i32) -> f64 {
+    0.0
 }
 
-pub fn climb_stairs(n: usize) -> usize {
-    0
+pub fn sort_k_sorted_array(arr: &mut [i32], k: i32) -> Vec<i32> {
+    vec![]
 }
 
 
 #[cfg(test)]
 mod tests {
-    use super::climb_stairs_dp;
-    use super::climb_stairs;
+    use super::*;
 
     #[test]
-    fn it_climb_stairs_dp() {
-        assert_eq!(climb_stairs_dp(0), 0);
-        assert_eq!(climb_stairs_dp(1), 1);
-        assert_eq!(climb_stairs_dp(2), 2);
-        assert_eq!(climb_stairs_dp(3), 3);
-        assert_eq!(climb_stairs_dp(4), 5);
-        assert_eq!(climb_stairs_dp(5), 8);
+    fn test_pow() {
+        assert_eq!(pow(4f64, 2), 16.0);
     }
 
     #[test]
-    fn it_climb_stairs() {
-        assert_eq!(climb_stairs(0), 0);
-        assert_eq!(climb_stairs(1), 1);
-        assert_eq!(climb_stairs(2), 2);
-        assert_eq!(climb_stairs(3), 3);
-        assert_eq!(climb_stairs(4), 5);
-        assert_eq!(climb_stairs(5), 8);
+    fn test_pow_case1() {
+        assert_eq!(pow(2f64, 4), 16.0);
+    }
+
+    #[test]
+    fn test_pow_case2() {
+        assert_eq!(pow(2f64, -2), 0.25);
+    }
+
+    #[test]
+    fn test_pow_case3() {
+        let epsilon = 1e-9;
+        let expected = 2.1_f64.powi(3);
+        println!("{}", expected);
+
+        let res = pow(2.1, 3);
+
+        assert!((res - expected).abs() < epsilon,
+                "Expected {} to be approximately equal to {}, but they differ by {}",
+                res,
+                expected,
+                (res - expected).abs()
+        );
+    }
+
+
+    #[test]
+    fn basic() {
+        let mut arr: Vec<i32> = vec![3, 2, 1, 5, 4, 7, 6, 5];
+
+        assert_eq!(sort_k_sorted_array(&mut arr, 3), vec![1, 2, 3, 4, 5, 5, 6, 7]);
     }
 }
