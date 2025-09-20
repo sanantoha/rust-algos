34f88d5df7797e67314113e3511f7d2f0ed9803d
AntonSakharovAE <anton@alefeducation.com>
2025-09-20 13:08:38 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,30 +1,30 @@
 
-pub fn min_number_of_jumps(arr: &[i32]) -> i32 {
-    0
-}
-
-pub fn min_number_of_jumps1(arr: &[i32]) -> i32 {
+pub fn count_substrings(src: &str) -> usize {
     0
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{min_number_of_jumps, min_number_of_jumps1};
 
-    const ARR: &[i32] = &[3, 4, 2, 1, 2, 3, 7, 1, 1, 1, 3];
+    use super::count_substrings;
 
     #[test]
-    fn test_min_number_of_jumps() {
-
-        let res = min_number_of_jumps(ARR);
-        println!("{}", res);
-        assert_eq!(res, 4);
+    fn test_count_substrings() {
+        assert_eq!(count_substrings("abc"), 3);
     }
 
     #[test]
-    fn test_min_number_of_jumps1() {
-        let res = min_number_of_jumps1(ARR);
-        println!("{}", res);
-        assert_eq!(res, 4);
+    fn test_count_substrings_case1() {
+        assert_eq!(count_substrings("aaa"), 6);
+    }
+
+    #[test]
+    fn test_count_substrings_case2() {
+        assert_eq!(count_substrings("aabbbaa"), 14);
+    }
+
+    #[test]
+    fn test_count_substrings_case3() {
+        assert_eq!(count_substrings("aaab"), 7);
     }
 }
