ccc0c27d8fb2ab3706b55e3e2ca04f6e235231d1
AntonSakharovAE <anton@alefeducation.com>
2025-09-21 12:32:14 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,30 +1,47 @@
+use crate::tree::TreeNode;
 
-pub fn count_substrings(src: &str) -> usize {
-    0
+pub fn pre_order_rec(root: &Option<Box<TreeNode>>) -> Vec<i32> {
+    vec![]
+}
+
+pub fn in_order_rec(root: &Option<Box<TreeNode>>) -> Vec<i32> {
+    vec![]
+}
+
+pub fn post_order_rec(root: &Option<Box<TreeNode>>) -> Vec<i32> {
+    vec![]
 }
 
 #[cfg(test)]
 mod tests {
-
-    use super::count_substrings;
+    use super::{pre_order_rec, in_order_rec, post_order_rec};
+    use crate::tree::TreeNode;
 
     #[test]
-    fn test_count_substrings() {
-        assert_eq!(count_substrings("abc"), 3);
+    pub fn test_dfs_pre_order_rec() {
+        assert_eq!(pre_order_rec(&create_tree()), vec![5, 2, 1, 3, 8, 7, 9]);
     }
 
     #[test]
-    fn test_count_substrings_case1() {
-        assert_eq!(count_substrings("aaa"), 6);
+    pub fn test_dfs_in_order_rec() {
+        assert_eq!(in_order_rec(&create_tree()), vec![1, 2, 3, 5, 7, 8, 9]);
     }
 
     #[test]
-    fn test_count_substrings_case2() {
-        assert_eq!(count_substrings("aabbbaa"), 14);
+    pub fn test_dfs_post_order_rec() {
+        assert_eq!(post_order_rec(&create_tree()), vec![1, 3, 2, 7, 9, 8, 5]);
     }
 
-    #[test]
-    fn test_count_substrings_case3() {
-        assert_eq!(count_substrings("aaab"), 7);
+    fn create_tree() -> Option<Box<TreeNode>> {
+        Some(
+            Box::new(TreeNode::new(5,
+                                   Some(Box::new(TreeNode::new(2,
+                                                               Some(Box::new(TreeNode::leaf(1))),
+                                                               Some(Box::new(TreeNode::leaf(3)))))),
+                                   Some(Box::new(TreeNode::new(8,
+                                                                              Some(Box::new(TreeNode::leaf(7))),
+                                                                              Some(Box::new(TreeNode::leaf(9)))))),
+            ))
+        )
     }
 }
