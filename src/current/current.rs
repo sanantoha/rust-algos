638d51a474e7a5c2c00accc3afda6cf8375c1254
AntonSakharovAE <anton@alefeducation.com>
2025-10-05 18:54:09 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,41 +1,16 @@
-use crate::graph::Digraph;
 
-pub fn sort_rec(graph: &Digraph) -> Result<Vec<i32>, String> {
-    Err("not implemented".to_owned())
+pub fn combination_sum(arr: &[i32], target: i32) -> Vec<Vec<i32>> {
+    vec![]
 }
-
-pub fn sort_iter(graph: &Digraph) -> Result<Vec<i32>, String> {
-    Err("not implemented".to_owned())
-}
-
 
 #[cfg(test)]
 mod tests {
-    use std::path::PathBuf;
-    use crate::graph::Digraph;
-    use super::{sort_iter, sort_rec};
+    use super::combination_sum;
 
     #[test]
-    fn test_sort_rec() {
+    fn it_combination_sum() {
+        let arr = &[2, 3, 5, 7];
 
-        if let Ok(graph) = Digraph::from_file(PathBuf::from("src/graph/digraph.txt")) {
-            println!("{}", graph);
-
-            let res = sort_rec(&graph).expect("sorting successfully");
-            println!("{:?}", res);
-            assert_eq!(res, vec![8, 9, 1, 0, 2, 3, 4, 5, 10, 11, 6, 7, 12, 13]);
-        }
-    }
-
-    #[test]
-    fn test_sort_iter() {
-
-        if let Ok(graph) = Digraph::from_file(PathBuf::from("src/graph/digraph.txt")) {
-            println!("{}", graph);
-
-            let res = sort_iter(&graph).expect("sorting successfully");
-            println!("{:?}", res);
-            assert_eq!(res, vec![0, 1, 8, 2, 9, 4, 3, 5, 6, 10, 7, 11, 12, 13]);
-        }
+        assert_eq!(combination_sum(arr, 7), vec![vec![2, 2, 3], vec![2, 5], vec![7]]);
     }
 }
