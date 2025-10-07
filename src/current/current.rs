23dd02c73167ca2e79ea0b0dcc95b80f2a1ee862
AntonSakharovAE <anton@alefeducation.com>
2025-10-07 08:55:07 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,39 +1,28 @@
 
-
-fn counting_sort(arr: &mut [i32]) {
-
+pub fn water_area(heights: &[i32]) -> i32 {
+    0
 }
+
+pub fn water_area1(heights: &[i32]) -> i32 {
+    0
+}
+
 
 #[cfg(test)]
 mod tests {
-    use rand::Rng;
-    use super::*;
+    use super::{water_area, water_area1};
+
+    const HEIGHTS: &[i32] = &[0, 8, 0, 0, 5, 0, 0, 10, 0, 0, 1, 1, 0, 3];
+
+    const EXP: i32 = 48;
 
     #[test]
-    fn test_counting_sort() {
-        let mut arr = vec![5, 2, 4, 6, 1, 3];
-
-        counting_sort(&mut arr);
-
-        assert_eq!(arr, [1, 2, 3, 4, 5, 6]);
+    fn test_water_area() {
+        assert_eq!(water_area(HEIGHTS), EXP)
     }
 
     #[test]
-    fn test_counting_sort_case1() {
-        let mut arr = vec![0; 30];
-
-        let mut rand = rand::rng();
-
-        for i in 0..arr.len() {
-            arr[i] = rand.random_range(0..100)
-        }
-
-        counting_sort(&mut arr);
-
-        println!("{:?}", arr);
-
-        for i in 1..arr.len() {
-            assert!(arr[i - 1] <= arr[i]);
-        }
+    fn test_water_area1() {
+        assert_eq!(water_area1(HEIGHTS), EXP)
     }
 }
