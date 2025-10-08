e63337d0a92c9bf9824e8941fcb5ac34b8ef3397
AntonSakharovAE <anton@alefeducation.com>
2025-10-08 07:04:16 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,28 +1,15 @@
 
-pub fn water_area(heights: &[i32]) -> i32 {
+pub fn max_profit(arr: &[i32]) -> i32 {
     0
 }
-
-pub fn water_area1(heights: &[i32]) -> i32 {
-    0
-}
-
 
 #[cfg(test)]
 mod tests {
-    use super::{water_area, water_area1};
-
-    const HEIGHTS: &[i32] = &[0, 8, 0, 0, 5, 0, 0, 10, 0, 0, 1, 1, 0, 3];
-
-    const EXP: i32 = 48;
+    use super::max_profit;
 
     #[test]
-    fn test_water_area() {
-        assert_eq!(water_area(HEIGHTS), EXP)
-    }
+    fn it_max_profit() {
 
-    #[test]
-    fn test_water_area1() {
-        assert_eq!(water_area1(HEIGHTS), EXP)
+        assert_eq!(max_profit(&vec![7, 1, 5, 3, 6, 4]), 5);
     }
 }
