d29d70fda843beb72287505e8609ecb548c5c5d8
AntonSakharovAE <anton@alefeducation.com>
2025-10-09 12:06:50 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,34 +1,32 @@
+use std::borrow::Borrow;
+use crate::tree::TreeNode;
 
-pub fn min_rewards(arr: &[i32]) -> i32 {
-    0
-}
-
-pub fn min_rewards1(arr: &[i32]) -> i32 {
-    0
-}
-
-pub fn min_rewards2(arr: &[i32]) -> i32 {
+pub fn evaluate_expression_tree(root: &Option<Box<TreeNode>>) -> i32 {
     0
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{min_rewards, min_rewards1, min_rewards2};
-
-    const ARR: &[i32] = &[8, 4, 2, 1, 3, 6, 7, 9, 5];
-
-    #[test]
-    fn it_min_rewards() {
-        assert_eq!(min_rewards(ARR), 25);
-    }
+    use crate::tree::TreeNode;
+    use super::evaluate_expression_tree;
 
     #[test]
-    fn it_min_rewards1() {
-        assert_eq!(min_rewards1(ARR), 25);
-    }
+    fn test_evaluate_expression_tree() {
 
-    #[test]
-    fn it_min_rewards2() {
-        assert_eq!(min_rewards2(ARR), 25);
+        let root = Some(Box::new(TreeNode::new(-1,
+                                               Some(Box::new(TreeNode::new(-2,
+                                                                           Some(Box::new(TreeNode::new(-4,
+                                                                                                       Some(Box::new(TreeNode::leaf(3))),
+                                                                                                       Some(Box::new(TreeNode::leaf(2)))
+                                                                           ))),
+                                                                           Some(Box::new(TreeNode::leaf(2)))
+                                               ))),
+                                               Some(Box::new(TreeNode::new(-3,
+                                                                           Some(Box::new(TreeNode::leaf(8))),
+                                                                           Some(Box::new(TreeNode::leaf(3)))
+                                               )))
+        )));
+
+        assert_eq!(evaluate_expression_tree(&root), 6);
     }
 }
