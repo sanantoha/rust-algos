db15e463ca417b1f3858cadbd309832a4b2c11b6
AntonSakharovAE <anton@alefeducation.com>
2025-10-10 08:39:30 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,32 +1,29 @@
-use std::borrow::Borrow;
 use crate::tree::TreeNode;
 
-pub fn evaluate_expression_tree(root: &Option<Box<TreeNode>>) -> i32 {
-    0
+pub fn left_view(root: Box<TreeNode>) -> Vec<i32> {
+    vec![]
 }
 
 #[cfg(test)]
 mod tests {
     use crate::tree::TreeNode;
-    use super::evaluate_expression_tree;
+    use super::left_view;
 
     #[test]
-    fn test_evaluate_expression_tree() {
+    fn it_left_view() {
+        let root = Box::new(TreeNode::new(1,
+                                          Some(Box::new(TreeNode::leaf(2))),
+                                          Some(Box::new(TreeNode::new(3,
+                                                                      Some(Box::new(TreeNode::new(4,
+                                                                                                  Some(Box::new(TreeNode::leaf(5))),
+                                                                                                  Some(Box::new(TreeNode::new(6,
+                                                                                                                              None,
+                                                                                                                              Some(Box::new(TreeNode::leaf(7))))))))),
+                                                                      Some(Box::new(TreeNode::leaf(8)))))),
+        ));
 
-        let root = Some(Box::new(TreeNode::new(-1,
-                                               Some(Box::new(TreeNode::new(-2,
-                                                                           Some(Box::new(TreeNode::new(-4,
-                                                                                                       Some(Box::new(TreeNode::leaf(3))),
-                                                                                                       Some(Box::new(TreeNode::leaf(2)))
-                                                                           ))),
-                                                                           Some(Box::new(TreeNode::leaf(2)))
-                                               ))),
-                                               Some(Box::new(TreeNode::new(-3,
-                                                                           Some(Box::new(TreeNode::leaf(8))),
-                                                                           Some(Box::new(TreeNode::leaf(3)))
-                                               )))
-        )));
-
-        assert_eq!(evaluate_expression_tree(&root), 6);
+        let res = left_view(root);
+        println!("{res:?}");
+        assert_eq!(res, vec![1, 2, 4, 5, 7]);
     }
 }
