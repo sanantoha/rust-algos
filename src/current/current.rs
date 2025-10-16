34d73f99ba18a86972b403ea73e6b401a55bbc77
AntonSakharovAE <anton@alefeducation.com>
2025-10-16 07:15:11 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,29 +1,28 @@
 
-pub fn valid_starting_city(distances: &[i32], fuel: &[i32], mpg: i32) -> Option<usize> {
-    None
+pub fn word_break(str: &str, word_dict: &[&str]) -> bool {
+    false
 }
 
-pub fn valid_starting_city1(distances: &[i32], fuel: &[i32], mpg: i32) -> Option<usize> {
-    None
-}
 
 #[cfg(test)]
 mod tests {
-    use super::valid_starting_city;
-    use super::valid_starting_city1;
-
-    const DISTANCES: &[i32] = &[5, 25, 15, 10, 15];
-    const FUEL: &[i32] = &[1, 2, 1, 0, 3];
-    const MPG: i32 = 10;
-    const EXP: usize = 4;
+    use super::word_break;
 
     #[test]
-    fn it_valid_starting_city() {
-        assert_eq!(valid_starting_city(DISTANCES, FUEL, MPG), Some(EXP));
+    fn it_word_break() {
+
+        assert!(word_break("leetcode", &["leet", "code"]));
     }
 
     #[test]
-    fn it_valid_starting_city1() {
-        assert_eq!(valid_starting_city1(DISTANCES, FUEL, MPG), Some(EXP));
+    fn it_word_break1() {
+
+        assert!(word_break("applepenapple", &["apple", "pen"]));
+    }
+
+    #[test]
+    fn it_word_break_impossible() {
+
+        assert!(!word_break("catsandog", &["cats", "dog", "sand", "and", "cat"]));
     }
 }
