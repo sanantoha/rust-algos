dd5d1ce126449a2962ca5d540cb54c4bd725089e
AntonSakharovAE <anton@alefeducation.com>
2025-10-20 07:32:55 +0400

    + 1 task completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,75 +1,40 @@
+use crate::list::ListNode;
 use std::cell::RefCell;
 use std::rc::Rc;
-use crate::list::ArbListNode;
 
-pub fn deep_copy(head: &Rc<RefCell<ArbListNode>>) -> Option<Rc<RefCell<ArbListNode>>> {
+
+pub fn middle_node(head: &Option<Rc<RefCell<ListNode>>>) -> Option<Rc<RefCell<ListNode>>> {
     None
 }
 
 
 #[cfg(test)]
 mod tests {
-
-    use super::deep_copy;
-    use crate::list::ArbListNode;
-    use std::{cell::RefCell, rc::Rc};
+    use super::middle_node;
     use crate::list::Displayable;
+    use crate::list::ListNode;
 
     #[test]
-    fn it_deep_copy() {
-        let root = ArbListNode::new(1);
-        let second = ArbListNode::new(2);
-        let third = ArbListNode::new(3);
-        let four = ArbListNode::new(4);
-        let five = ArbListNode::new(5);
+    pub fn test_middle_node() {
+        let head = Some(ListNode::with_next(4, Some(ListNode::with_next(8, Some(ListNode::with_next(15, Some(ListNode::new(19))))))));
+        let exp = Some(ListNode::with_next(15, Some(ListNode::new(19))));
 
-        root.borrow_mut().next = Some(Rc::clone(&second));
-        second.borrow_mut().next = Some(Rc::clone(&third));
-        third.borrow_mut().next = Some(Rc::clone(&four));
-        four.borrow_mut().next = Some(Rc::clone(&five));
-
-        second.borrow_mut().arb = Some(Rc::clone(&five));
-        third.borrow_mut().arb = Some(Rc::clone(&root));
-        five.borrow_mut().arb = Some(Rc::clone(&second));
-
-
-        // ArbitraryListNode copy = deepCopy(root);
-        let copy = deep_copy(&root);
-        assert!(copy.is_some());
-        if let Some(node) = copy {
-            println!("root: {}", Displayable::new(Rc::clone(&root)));
-            println!("copy: {}", Displayable::new(Rc::clone(&node)));
-            assert_arb_list_node(&root, &node);
+        let res = middle_node(&head);
+        if let Some(display_res) = Displayable::from_option(res.clone()) {
+            println!("{}", display_res);
         }
-
+        assert_eq!(middle_node(&head), exp);
     }
 
-    fn assert_arb_list_node(root: &Rc<RefCell<ArbListNode>>, copy: &Rc<RefCell<ArbListNode>>) {
+    #[test]
+    pub fn test_middle_node_odd_list() {
+        let head1 = Some(ListNode::with_next(1, Some(ListNode::with_next(2, Some(ListNode::with_next(3, Some(ListNode::with_next(4, Some(ListNode::new(5))))))))));
+        let exp1 = Some(ListNode::with_next(3, Some(ListNode::with_next(4, Some(ListNode::new(5))))));
 
-        let mut curr = Some(Rc::clone(root));
-        let mut curr_copy = Some(Rc::clone(copy));
-
-        while let (Some(node), Some(node_copy)) = (curr.take(), curr_copy.take()) {
-
-            assert_eq!(node.borrow().val, node_copy.borrow().val); // copied value should be the same
-            assert!(!Rc::ptr_eq(&node, &node_copy)); // failed if the same pointer means copy was incorrect
-
-            let arb = node.borrow().arb.as_ref().map(Rc::clone);
-            let arb_copy = node_copy.borrow().arb.as_ref().map(Rc::clone);
-            assert_eq!(arb.is_none(), arb_copy.is_none()); // if arbitrary pointer is not present it should be the same in the copy
-            assert_eq!(arb.is_some(), arb_copy.is_some()); // if artibrary pointer is present it should be the same in the copy
-
-            if let (Some(arb), Some(arb_copy)) = (arb, arb_copy) {
-                assert_eq!(arb.borrow().val, arb_copy.borrow().val); // arbitrary value should be the same
-                assert!(!Rc::ptr_eq(&arb, &arb_copy)); // failed if copy arbitrary pointer is the same as original
-            }
-
-            curr = node.borrow().next.as_ref().map(Rc::clone);
-            curr_copy = node_copy.borrow().next.as_ref().map(Rc::clone);
+        let res1 = middle_node(&head1);
+        if let Some(display_res) = Displayable::from_option(res1.clone()) {
+            println!("{}", display_res);
         }
-
-        assert!(curr.is_none()); // list should have the same length and ended after iterate all node
-        assert!(curr_copy.is_none()); // copy list should also ended after iteration because main list and copy should have the same amount of nodes
-
+        assert_eq!(middle_node(&head1), exp1);
     }
 }
