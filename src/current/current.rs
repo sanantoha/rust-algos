bf350f19aa57e457848b01d97d816b25b018f98e
AntonSakharovAE <anton@alefeducation.com>
2025-10-29 09:50:16 +0400

    + 1 task added

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,31 +1,43 @@
 
-pub fn reverse(i: i32) -> i32 {
+pub fn lnds(arr: &[i32]) -> i32 {
     0
 }
 
+pub fn lnds1(arr: &[i32]) -> i32 {
+    0
+}
+
+pub fn lnds_list(arr: &[i32]) -> Vec<i32> {
+    vec![]
+}
+
+pub fn lnds_list1(arr: &[i32]) -> Vec<i32> {
+    vec![]
+}
 
 #[cfg(test)]
 mod tests {
 
-    use super::reverse;
+    use super::{lnds, lnds1, lnds_list, lnds_list1};
 
+    const ARR: &[i32] = &[-2, -1, 2, 3, 4, 5, 2, 2, 2, 2, 3];
     #[test]
-    fn test_reverse() {
-        assert_eq!(reverse(123), 321);
+    fn test_lnds() {
+        assert_eq!(lnds(ARR), 8);
     }
 
     #[test]
-    fn test_reverse_case1() {
-        assert_eq!(reverse(0), 0);
+    fn test_lnds1() {
+        assert_eq!(lnds1(ARR), 8);
     }
 
     #[test]
-    fn test_reverse_case2() {
-        assert_eq!(reverse(-123), -321);
+    fn test_lnds_list() {
+        assert_eq!(lnds_list(ARR), vec![-2, -1, 2, 2, 2, 2, 2, 3]);
     }
 
     #[test]
-    fn test_reverse_case3() {
-        assert_eq!(reverse(120), 21);
+    fn test_lnds_list1() {
+        assert_eq!(lnds_list1(ARR), vec![-2, -1, 2, 2, 2, 2, 2, 3]);
     }
 }
