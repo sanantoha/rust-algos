0aca647302335c0fcf4540c5be656ee79c975132
sanantoha <sanantoha@gmail.com>
2025-11-08 10:42:38 +0400

    +1 completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,15 +1,24 @@
 
-pub fn permute(src: &str) -> Vec<String> {
+pub fn top_k_frequent<'a>(_words: &[&'a str], _k: i32) -> Vec<&'a str> {
     vec![]
 }
 
 #[cfg(test)]
 mod tests {
 
-    use super::permute;
+    use super::top_k_frequent;
 
     #[test]
-    fn test_string_permutation() {
-        assert_eq!(permute("abc"), vec!["abc", "acb", "bac", "bca", "cab", "cba"]);
+    pub fn test_top_k_frequent() {
+        let words = vec!["i","love","leetcode","i","love","coding"];
+
+        assert_eq!(top_k_frequent(&words, 2), vec!["i", "love"]);
+    }
+
+    #[test]
+    pub fn test_top_k_frequent_for_4() {
+        let words = vec!["the","day","is","sunny","the","the","the","sunny","is","is"];
+
+        assert_eq!(top_k_frequent(&words, 4), vec!["the","is","sunny","day"]);
     }
 }
