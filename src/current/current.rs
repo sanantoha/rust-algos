63305ddd16839c4c1c1cae7209ec9dda0c19aa47
sanantoha <sanantoha@gmail.com>
2025-11-16 09:16:38 +0400

    +1 completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,31 +1,38 @@
-use std::collections::HashMap;
-use std::rc::Rc;
-use crate::graph::EdgeT;
 
-pub fn bfs(_graph: &HashMap<String, Vec<Rc<EdgeT<String>>>>, _start: String) -> Vec<String> {
-    vec![]
+pub fn gcd(mut a: i32, mut b: i32) -> i32 {
+    0
 }
 
 #[cfg(test)]
 mod tests {
-    use std::path::PathBuf;
-    use super::bfs;
-    use crate::graph::{graph_from_file, graph_to_string};
+    use super::gcd;
 
 
     #[test]
-    fn it_bfs() {
-        if let Ok(graph) = graph_from_file(PathBuf::from("src/graph/bfs.txt")) {
-            let graph_str = graph_to_string(&graph);
-            println!("{}", graph_str);
+    fn test_gcd() {
+        let res = gcd(18, 6);
+        println!("{res}");
+        assert_eq!(res, 6);
+    }
 
-            let exp: Vec<String> = vec![
-                0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24
-            ].iter().map(|s| s.to_string()).collect();
+    #[test]
+    fn test_gcd1() {
+        let res = gcd(18, 10);
+        println!("{res}");
+        assert_eq!(res, 2);
+    }
 
-            let res = bfs(&graph, "0".to_owned());
-            println!("{:?}", res);
-            assert_eq!(res, exp);
-        }
+    #[test]
+    fn test_gcd2() {
+        let res = gcd(17, 11);
+        println!("{res}");
+        assert_eq!(res, 1);
+    }
+
+    #[test]
+    fn test_gcd3() {
+        let res = gcd(5, 15);
+        println!("{res}");
+        assert_eq!(res, 5);
     }
 }
