f51098467620c27e52894a4b4f25b3318a0f1b66
sanantoha <sanantoha@gmail.com>
2025-11-19 08:30:10 +0400

    +1 completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,14 +1,61 @@
+use crate::tree::TreeNode;
 
-pub fn min_number_of_coins_for_change(n: usize, denoms: &[usize]) -> i32 {
+pub fn find_tilt(root: &Option<Box<TreeNode>>) -> i32 {
     0
 }
 
+
 #[cfg(test)]
 mod tests {
-    use super::min_number_of_coins_for_change;
+    use crate::tree::TreeNode;
+    use super::find_tilt;
 
     #[test]
-    fn test_min_number_of_coins_for_change() {
-        assert_eq!(min_number_of_coins_for_change(7, &[1, 5, 10]), 3);
+    fn it_find_tilt() {
+        let root = Some(Box::new(TreeNode::new(1, Some(Box::new(TreeNode::leaf(2))), Some(Box::new(TreeNode::leaf(3))))));
+
+        assert_eq!(find_tilt(&root), 1);
+    }
+
+    #[test]
+    fn it_find_tilt1() {
+        let root = Some(Box::new(TreeNode::new(
+            4,
+            Some(Box::new(TreeNode::new(
+                2,
+                Some(Box::new(TreeNode::leaf(3))),
+                Some(Box::new(TreeNode::leaf(5))),
+            ))),
+            Some(Box::new(TreeNode::new(
+                9,
+                None,
+                Some(Box::new(TreeNode::leaf(7))),
+            ))),
+        )));
+
+        assert_eq!(find_tilt(&root), 15);
+    }
+
+    #[test]
+    fn it_find_titl2() {
+        let root = Some(Box::new(TreeNode::new(
+            21,
+            Some(Box::new(TreeNode::new(
+                7,
+                Some(Box::new(TreeNode::new(
+                    1,
+                    Some(Box::new(TreeNode::leaf(3))),
+                    Some(Box::new(TreeNode::leaf(3))),
+                ))),
+                Some(Box::new(TreeNode::leaf(1))),
+            ))),
+            Some(Box::new(TreeNode::new(
+                14,
+                Some(Box::new(TreeNode::leaf(2))),
+                Some(Box::new(TreeNode::leaf(2))),
+            ))),
+        )));
+
+        assert_eq!(find_tilt(&root), 9);
     }
 }
