ab2d987515fd13993ed5a90e96d42f1c3f5b842c
sanantoha <sanantoha@gmail.com>
2025-11-20 09:13:18 +0400

    +1 completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,61 +1,31 @@
-use crate::tree::TreeNode;
 
-pub fn find_tilt(root: &Option<Box<TreeNode>>) -> i32 {
+pub fn count_substrings(s: &str) -> usize {
     0
 }
 
 
 #[cfg(test)]
 mod tests {
-    use crate::tree::TreeNode;
-    use super::find_tilt;
+
+    use super::count_substrings;
 
     #[test]
-    fn it_find_tilt() {
-        let root = Some(Box::new(TreeNode::new(1, Some(Box::new(TreeNode::leaf(2))), Some(Box::new(TreeNode::leaf(3))))));
-
-        assert_eq!(find_tilt(&root), 1);
+    fn test_count_substrings() {
+        assert_eq!(count_substrings("abc"), 3);
     }
 
     #[test]
-    fn it_find_tilt1() {
-        let root = Some(Box::new(TreeNode::new(
-            4,
-            Some(Box::new(TreeNode::new(
-                2,
-                Some(Box::new(TreeNode::leaf(3))),
-                Some(Box::new(TreeNode::leaf(5))),
-            ))),
-            Some(Box::new(TreeNode::new(
-                9,
-                None,
-                Some(Box::new(TreeNode::leaf(7))),
-            ))),
-        )));
-
-        assert_eq!(find_tilt(&root), 15);
+    fn test_count_substrings_case1() {
+        assert_eq!(count_substrings("aaa"), 6);
     }
 
     #[test]
-    fn it_find_titl2() {
-        let root = Some(Box::new(TreeNode::new(
-            21,
-            Some(Box::new(TreeNode::new(
-                7,
-                Some(Box::new(TreeNode::new(
-                    1,
-                    Some(Box::new(TreeNode::leaf(3))),
-                    Some(Box::new(TreeNode::leaf(3))),
-                ))),
-                Some(Box::new(TreeNode::leaf(1))),
-            ))),
-            Some(Box::new(TreeNode::new(
-                14,
-                Some(Box::new(TreeNode::leaf(2))),
-                Some(Box::new(TreeNode::leaf(2))),
-            ))),
-        )));
+    fn test_count_substrings_case2() {
+        assert_eq!(count_substrings("aabbbaa"), 14);
+    }
 
-        assert_eq!(find_tilt(&root), 9);
+    #[test]
+    fn test_count_substrings_case3() {
+        assert_eq!(count_substrings("aaab"), 7);
     }
 }
