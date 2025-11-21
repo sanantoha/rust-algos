5b373aabcf0c4e493af068d4bd01aca7405e4495
sanantoha <sanantoha@gmail.com>
2025-11-21 08:51:53 +0400

    +1 completed

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,31 +1,63 @@
 
-pub fn count_substrings(s: &str) -> usize {
+pub fn staircase_traversal_rec(height: usize, max_steps: usize) -> i32 {
+    0
+}
+
+pub fn staircase_traversal_rec_memoization(height: usize, max_steps: usize) -> i32 {
     0
 }
 
 
+pub fn staircase_traversal_iter(height: usize, max_steps: usize) -> i32 {
+    0
+}
+
+
+pub fn staircase_traversal_sliding_window(height: usize, max_steps: usize) -> i32 {
+    0
+}
+
 #[cfg(test)]
 mod tests {
-
-    use super::count_substrings;
+    use super::{staircase_traversal_iter, staircase_traversal_rec, staircase_traversal_rec_memoization, staircase_traversal_sliding_window};
 
     #[test]
-    fn test_count_substrings() {
-        assert_eq!(count_substrings("abc"), 3);
+    fn test_staircase_traversal_rec() {
+        assert_eq!(staircase_traversal_rec(4, 2), 5);
     }
 
     #[test]
-    fn test_count_substrings_case1() {
-        assert_eq!(count_substrings("aaa"), 6);
+    fn test_staircase_traversal_rec_case1() {
+        assert_eq!(staircase_traversal_rec(4, 3), 7);
     }
 
     #[test]
-    fn test_count_substrings_case2() {
-        assert_eq!(count_substrings("aabbbaa"), 14);
+    fn test_staircase_traversal_rec_memoization() {
+        assert_eq!(staircase_traversal_rec_memoization(4, 2), 5);
     }
 
     #[test]
-    fn test_count_substrings_case3() {
-        assert_eq!(count_substrings("aaab"), 7);
+    fn test_staircase_traversal_rec_memoization_case1() {
+        assert_eq!(staircase_traversal_rec_memoization(4, 3), 7);
+    }
+
+    #[test]
+    fn test_staircase_traversal_iter() {
+        assert_eq!(staircase_traversal_iter(4, 2), 5);
+    }
+
+    #[test]
+    fn test_staircase_traversal_iter_case1() {
+        assert_eq!(staircase_traversal_iter(4, 3), 7);
+    }
+
+    #[test]
+    fn test_staircase_traversal_sliding_window() {
+        assert_eq!(staircase_traversal_sliding_window(4, 2), 5);
+    }
+
+    #[test]
+    fn test_staircase_traversal_sliding_window_case1() {
+        assert_eq!(staircase_traversal_sliding_window(4, 3), 7);
     }
 }
