3cb5e967a7efd4286ba582cf95963f1311f1064d
sanantoha <sanantoha@gmail.com>
2025-11-26 07:28:57 +0400

    +1 added

--- src/graph/zero_one_matrix.rs
+++ src/graph/zero_one_matrix.rs
@@ -1,6 +1,64 @@
+use std::collections::VecDeque;
 
-pub fn update_matrix(_matrix: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-    vec![]
+// O(w * h) time | O(w * h) space
+pub fn update_matrix(matrix: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+    let mut matrix_mv = create_matrix_replace_one_on_max_val(matrix);
+
+    let mut queue = find_all_zeros(&matrix_mv);
+
+    while let Some((row, col, level)) = queue.pop_front() {
+
+        for (nrow, ncol) in get_neighborns(&matrix_mv, row, col) {
+            if matrix_mv[nrow][ncol] == i32::MAX {
+                queue.push_back((nrow, ncol, level + 1));
+            }
+            matrix_mv[nrow][ncol] = matrix_mv[nrow][ncol].min(level + 1);
+        }
+    }
+
+    println!("{:?}", matrix_mv);
+    matrix_mv
+}
+
+fn get_neighborns(matrix: &Vec<Vec<i32>>, row: usize, col: usize) -> Vec<(usize, usize)> {
+    let mut res = vec![];
+    if row > 0 {
+        res.push((row - 1, col));
+    }
+    if col > 0 {
+        res.push((row, col - 1));
+    }
+    if row + 1 < matrix.len() {
+        res.push((row + 1, col));
+    }
+    if col + 1 < matrix[row].len() {
+        res.push((row, col + 1));
+    }
+    res
+}
+
+fn find_all_zeros(matrix: &Vec<Vec<i32>>) -> VecDeque<(usize, usize, i32)> {
+    let mut res = VecDeque::new();
+    for row in 0..matrix.len() {
+        for col in 0..matrix[row].len() {
+            if matrix[row][col] == 0 {
+                res.push_back((row, col, 0));
+            }
+        }
+    }
+    res
+}
+
+fn create_matrix_replace_one_on_max_val(matrix: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+    let mut res = matrix.to_vec();
+    for row in 0..matrix.len() {
+        for col in 0..matrix[row].len() {
+            if matrix[row][col] == 1 {
+                res[row][col] = i32::MAX;
+            }
+        }
+    }
+    res
 }
 
 
