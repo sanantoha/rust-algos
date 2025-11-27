f86c39729408ccc061a92e24e575045eff258113
sanantoha <sanantoha@gmail.com>
2025-11-27 12:30:42 +0400

    +1 added

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,39 +1,21 @@
 
+pub fn balanced_brackets(_str: &str) -> bool {
 
-pub fn rotate(arr: &mut Vec<Vec<i32>>) {
-
+    false
 }
-
 
 #[cfg(test)]
 mod tests {
 
-    use super::rotate;
+    use super::balanced_brackets;
 
     #[test]
-    fn it_rotate() {
+    fn it_balanced_brackets() {
+        assert!(balanced_brackets("([])(){}(())()()"));
+    }
 
-        /*
-    1 2 3  =>  1 4 7  => 7 4 1
-    4 5 6  =>  2 5 8  => 8 5 2
-    7 8 9  =>  3 6 9  => 9 6 3
-        */
-
-        let mut matrix = vec![
-            vec![1, 2, 3],
-            vec![4, 5, 6],
-            vec![7, 8, 9]
-        ];
-
-        let exp = vec![
-            vec![7, 4, 1],
-            vec![8, 5, 2],
-            vec![9, 6, 3]
-        ];
-
-        rotate(&mut matrix);
-
-        println!("{:?}", matrix);
-        assert_eq!(matrix, exp)
+    #[test]
+    fn it_im_balanced_brackets() {
+        assert!(!balanced_brackets("}{"));
     }
 }
