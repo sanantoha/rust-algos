0894face8653afca2b872b071083fdb27974eacd
sanantoha <sanantoha@gmail.com>
2025-12-04 07:45:23 +0400

    +1 added

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,43 +1,128 @@
+use crate::tree::TreeNode;
 
-pub fn lnds(arr: &[i32]) -> i32 {
+pub fn max_depth_rec(root: &Option<Box<TreeNode>>) -> i32 {
     0
 }
 
-pub fn lnds1(arr: &[i32]) -> i32 {
+pub fn max_depth_bfs(root: &Option<Box<TreeNode>>) -> i32 {
     0
 }
 
-pub fn lnds_list(arr: &[i32]) -> Vec<i32> {
-    vec![]
-}
-
-pub fn lnds_list1(arr: &[i32]) -> Vec<i32> {
-    vec![]
+pub fn max_depth_dfs_iter(root: &Option<Box<TreeNode>>) -> i32 {
+    0
 }
 
 #[cfg(test)]
 mod tests {
+    use super::{max_depth_bfs, max_depth_dfs_iter, max_depth_rec};
+    use crate::tree::TreeNode;
 
-    use super::{lnds, lnds1, lnds_list, lnds_list1};
-
-    const ARR: &[i32] = &[-2, -1, 2, 3, 4, 5, 2, 2, 2, 2, 3];
     #[test]
-    fn test_lnds() {
-        assert_eq!(lnds(ARR), 8);
+    fn test_max_depth_rec() {
+        let root = &create_tree();
+
+        assert_eq!(max_depth_rec(root), 4);
     }
 
     #[test]
-    fn test_lnds1() {
-        assert_eq!(lnds1(ARR), 8);
+    fn test_max_depth_rec_case1() {
+        let root = &create_tree1();
+
+        assert_eq!(max_depth_rec(root), 3);
     }
 
     #[test]
-    fn test_lnds_list() {
-        assert_eq!(lnds_list(ARR), vec![-2, -1, 2, 2, 2, 2, 2, 3]);
+    fn test_max_depth_rec_case2() {
+        let root = &create_tree2();
+
+        assert_eq!(max_depth_rec(root), 2);
     }
 
     #[test]
-    fn test_lnds_list1() {
-        assert_eq!(lnds_list1(ARR), vec![-2, -1, 2, 2, 2, 2, 2, 3]);
+    fn test_max_depth_bfs() {
+        let root = &create_tree();
+
+        assert_eq!(max_depth_bfs(root), 4);
+    }
+
+    #[test]
+    fn test_max_depth_bfs_case1() {
+        let root = &create_tree1();
+
+        assert_eq!(max_depth_bfs(root), 3);
+    }
+
+    #[test]
+    fn test_max_depth_bfs_case2() {
+        let root = &create_tree2();
+
+        assert_eq!(max_depth_bfs(root), 2);
+    }
+
+    #[test]
+    fn test_max_depth_dfs_iter() {
+        let root = &create_tree();
+
+        assert_eq!(max_depth_dfs_iter(root), 4);
+    }
+
+    #[test]
+    fn test_max_depth_dfs_iter_case1() {
+        let root = &create_tree1();
+
+        assert_eq!(max_depth_dfs_iter(root), 3);
+    }
+
+    #[test]
+    fn test_max_depth_dfs_iter_case2() {
+        let root = &create_tree2();
+
+        assert_eq!(max_depth_dfs_iter(root), 2);
+    }
+
+    fn create_tree() -> Option<Box<TreeNode>> {
+        let root = TreeNode::new(
+            5,
+            Some(Box::new(TreeNode::new(
+                2,
+                Some(Box::new(TreeNode::leaf(1))),
+                None,
+            ))),
+            Some(Box::new(TreeNode::new(
+                10,
+                Some(Box::new(TreeNode::leaf(7))),
+                Some(Box::new(TreeNode::new(
+                    20,
+                    None,
+                    Some(Box::new(TreeNode::leaf(25))),
+                ))),
+            ))),
+        );
+
+        Some(Box::new(root))
+    }
+
+    fn create_tree1() -> Option<Box<TreeNode>> {
+        let root = TreeNode::new(
+            3,
+            Some(Box::new(TreeNode::leaf(9))),
+            Some(Box::new(TreeNode::new(
+                20,
+                Some(Box::new(TreeNode::leaf(15))),
+                Some(Box::new(TreeNode::leaf(7))),
+            ))),
+        );
+
+        Some(Box::new(root))
+    }
+
+    fn create_tree2() -> Option<Box<TreeNode>> {
+        let root = TreeNode::new(
+            1,
+            None,
+            Some(Box::new(TreeNode::leaf(2))),
+        );
+
+        Some(Box::new(root))
     }
 }
