95043500d18309bf5b426d9ee0479a8c70583237
sanantoha <sanantoha@gmail.com>
2025-12-06 12:07:05 +0400

    +1 added

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,128 +1,79 @@
-use crate::tree::TreeNode;
+use std::cell::RefCell;
+use std::rc::Rc;
+use crate::graph::Node;
 
-pub fn max_depth_rec(root: &Option<Box<TreeNode>>) -> i32 {
-    0
-}
-
-pub fn max_depth_bfs(root: &Option<Box<TreeNode>>) -> i32 {
-    0
-}
-
-pub fn max_depth_dfs_iter(root: &Option<Box<TreeNode>>) -> i32 {
-    0
+pub fn clone_graph(node: Rc<RefCell<Node>>) -> Option<Rc<RefCell<Node>>> {
+    None
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{max_depth_bfs, max_depth_dfs_iter, max_depth_rec};
-    use crate::tree::TreeNode;
+    use std::cell::RefCell;
+    use std::rc::Rc;
+    use super::clone_graph;
+    use crate::graph::Node;
 
     #[test]
-    fn test_max_depth_rec() {
-        let root = &create_tree();
+    fn test_clone_graph() {
+        let node1 = Rc::new(RefCell::new(Node::new(1)));
+        let node2 = Rc::new(RefCell::new(Node::new(2)));
+        let node3 = Rc::new(RefCell::new(Node::new(3)));
+        let node4 = Rc::new(RefCell::new(Node::new(4)));
 
-        assert_eq!(max_depth_rec(root), 4);
-    }
+        node1.borrow_mut().neighbors.push(Rc::clone(&node2));
+        node1.borrow_mut().neighbors.push(Rc::clone(&node4));
 
-    #[test]
-    fn test_max_depth_rec_case1() {
-        let root = &create_tree1();
+        node2.borrow_mut().neighbors.push(Rc::clone(&node1));
+        node2.borrow_mut().neighbors.push(Rc::clone(&node3));
 
-        assert_eq!(max_depth_rec(root), 3);
-    }
+        node3.borrow_mut().neighbors.push(Rc::clone(&node2));
+        node3.borrow_mut().neighbors.push(Rc::clone(&node4));
 
-    #[test]
-    fn test_max_depth_rec_case2() {
-        let root = &create_tree2();
+        node4.borrow_mut().neighbors.push(Rc::clone(&node1));
+        node4.borrow_mut().neighbors.push(Rc::clone(&node3));
 
-        assert_eq!(max_depth_rec(root), 2);
-    }
+        let cloned_graph = clone_graph(Rc::clone(&node1));
 
-    #[test]
-    fn test_max_depth_bfs() {
-        let root = &create_tree();
+        match &cloned_graph {
+            Some(copy_node1) => {
+                assert_eq!(copy_node1.borrow().val, 1);
+                assert!(!Rc::ptr_eq(copy_node1, &node1));
+                assert_eq!(copy_node1.borrow().neighbors.len(), 2);
 
-        assert_eq!(max_depth_bfs(root), 4);
-    }
+                let copy_node2 = Rc::clone(copy_node1.borrow().neighbors.get(0).unwrap());
+                let copy_node4 = Rc::clone(copy_node1.borrow().neighbors.get(1).unwrap());
 
-    #[test]
-    fn test_max_depth_bfs_case1() {
-        let root = &create_tree1();
+                assert_eq!(copy_node2.borrow().val, 2);
+                assert!(!Rc::ptr_eq(&copy_node2, &node2));
+                assert_eq!(copy_node2.borrow().neighbors.len(), 2);
 
-        assert_eq!(max_depth_bfs(root), 3);
-    }
+                assert_eq!(copy_node4.borrow().val, 4);
+                assert!(!Rc::ptr_eq(&copy_node4, &node4));
+                assert_eq!(copy_node4.borrow().neighbors.len(), 2);
 
-    #[test]
-    fn test_max_depth_bfs_case2() {
-        let root = &create_tree2();
+                let copy_node11 = Rc::clone(copy_node2.borrow().neighbors.get(0).unwrap());
+                let copy_node3 = Rc::clone(copy_node2.borrow().neighbors.get(1).unwrap());
 
-        assert_eq!(max_depth_bfs(root), 2);
-    }
+                assert_eq!(copy_node11.borrow().val, 1);
+                assert!(!Rc::ptr_eq(&copy_node11, &node1));
+                assert!(Rc::ptr_eq(&copy_node11, &copy_node1));
+                assert_eq!(copy_node11.borrow().neighbors.len(), 2);
 
-    #[test]
-    fn test_max_depth_dfs_iter() {
-        let root = &create_tree();
+                assert_eq!(copy_node3.borrow().val, 3);
+                assert!(!Rc::ptr_eq(&copy_node3, &node3));
+                assert_eq!(copy_node3.borrow().neighbors.len(), 2);
 
-        assert_eq!(max_depth_dfs_iter(root), 4);
-    }
+                let copy_node22 = Rc::clone(copy_node3.borrow().neighbors.get(0).unwrap());
+                let copy_node44 = Rc::clone(copy_node3.borrow().neighbors.get(1).unwrap());
+                assert!(Rc::ptr_eq(&copy_node22, &copy_node2));
+                assert!(Rc::ptr_eq(&copy_node44, &copy_node4));
 
-    #[test]
-    fn test_max_depth_dfs_iter_case1() {
-        let root = &create_tree1();
-
-        assert_eq!(max_depth_dfs_iter(root), 3);
-    }
-
-    #[test]
-    fn test_max_depth_dfs_iter_case2() {
-        let root = &create_tree2();
-
-        assert_eq!(max_depth_dfs_iter(root), 2);
-    }
-
-    fn create_tree() -> Option<Box<TreeNode>> {
-        let root = TreeNode::new(
-            5,
-            Some(Box::new(TreeNode::new(
-                2,
-                Some(Box::new(TreeNode::leaf(1))),
-                None,
-            ))),
-            Some(Box::new(TreeNode::new(
-                10,
-                Some(Box::new(TreeNode::leaf(7))),
-                Some(Box::new(TreeNode::new(
-                    20,
-                    None,
-                    Some(Box::new(TreeNode::leaf(25))),
-                ))),
-            ))),
-        );
-
-        Some(Box::new(root))
-    }
-
-    fn create_tree1() -> Option<Box<TreeNode>> {
-        let root = TreeNode::new(
-            3,
-            Some(Box::new(TreeNode::leaf(9))),
-            Some(Box::new(TreeNode::new(
-                20,
-                Some(Box::new(TreeNode::leaf(15))),
-                Some(Box::new(TreeNode::leaf(7))),
-            ))),
-        );
-
-        Some(Box::new(root))
-    }
-
-    fn create_tree2() -> Option<Box<TreeNode>> {
-        let root = TreeNode::new(
-            1,
-            None,
-            Some(Box::new(TreeNode::leaf(2))),
-        );
-
-        Some(Box::new(root))
+                let copy_node33 = Rc::clone(copy_node4.borrow().neighbors.get(1).unwrap());
+                assert!(Rc::ptr_eq(&copy_node33, &copy_node3));
+            }
+            None => {
+                assert!(false);
+            }
+        }
     }
 }
