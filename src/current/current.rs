a2171022de82bae21b03f67e98cc0222aa16f141
sanantoha <sanantoha@gmail.com>
2025-12-09 07:45:37 +0400

    +1 added

--- src/current/current.rs
+++ src/current/current.rs
@@ -4,20 +4,6 @@ pub fn subarray_sort(arr: &[i32]) -> Option<(usize, usize)> {
     None
 }
 
-fn is_out_of_order(arr: &[i32], idx: usize) -> bool {
-    if arr.len() <= 1 {
-        return false;
-    }
-
-    if idx == 0 {
-        arr[idx] > arr[idx + 1]
-    } else if idx == arr.len() - 1 {
-        arr[idx - 1] > arr[idx]
-    } else {
-        arr[idx - 1] > arr[idx] || arr[idx] > arr[idx + 1]
-    }
-}
-
 #[cfg(test)]
 mod tests {
 
