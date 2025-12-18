ca42f999ceb375372fe86738d887060b1500695a
sanantoha <sanantoha@gmail.com>
2025-12-18 13:28:25 +0400

    +1 completed.

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,19 +1,20 @@
 
-
-pub fn valid_ip_addresses(str: &str) -> Vec<String> {
-    vec![]
+pub fn unique_path_iii(grid: &mut Vec<Vec<i32>>) -> i32 {
+    0
 }
 
 #[cfg(test)]
 mod tests {
-    use super::valid_ip_addresses;
+    use super::unique_path_iii;
 
     #[test]
-    fn test_valid_ip_addresses() {
-        let s = String::from("1921680");
+    fn it_unique_path_iii() {
+        let mut grid = vec![
+            vec![1, 0, 0, 0],
+            vec![0, 0, 0, 0],
+            vec![0, 0, 2, -1]
+        ];
 
-        let res = valid_ip_addresses(&s);
-        println!("{:?}", res);
-        assert_eq!(res, ["1.9.216.80", "1.92.16.80", "1.92.168.0", "19.2.16.80", "19.2.168.0", "19.21.6.80", "19.21.68.0", "19.216.8.0", "192.1.6.80", "192.1.68.0", "192.16.8.0"]);
+        assert_eq!(unique_path_iii(&mut grid), 2);
     }
 }
