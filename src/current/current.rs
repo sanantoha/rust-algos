ea3c9dbe5ccd44b5e4a523576f107807d187a495
sanantoha <sanantoha@gmail.com>
2025-12-20 10:19:25 +0400

    +1 completed.

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,21 +1,25 @@
 
-pub fn sqrt(n: i32) -> i32 {
-
-    -1
+pub fn permute(arr: &mut Vec<i32>) -> Vec<Vec<i32>> {
+    vec![]
 }
 
 #[cfg(test)]
 mod tests {
 
-    use super::sqrt;
+    use super::permute;
 
     #[test]
-    fn test_sqrt() {
-        assert_eq!(sqrt(16), 4);
-    }
+    fn it_permute() {
+        let mut arr = vec![1, 2, 3];
+        let exp_res = vec![
+            vec![1, 2, 3],
+            vec![1, 3, 2],
+            vec![2, 1, 3],
+            vec![2, 3, 1],
+            vec![3, 2, 1],
+            vec![3, 1, 2]
+        ];
 
-    #[test]
-    fn test_sqrt_case1() {
-        assert_eq!(sqrt(8), 2);
+        assert_eq!(permute(&mut arr), exp_res);
     }
 }
