bcfbcb78ff23fb6e7f26643247c5f025c0524059
sanantoha <sanantoha@gmail.com>
2025-12-21 11:38:44 +0400

    +1 completed.

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,25 +1,77 @@
 
-pub fn permute(arr: &mut Vec<i32>) -> Vec<Vec<i32>> {
-    vec![]
+pub fn rotate(arr: &mut [i32], k: i32) {
+    
+}
+
+pub fn rotate1(arr: &mut [i32], k: i32) {
+    
 }
 
 #[cfg(test)]
 mod tests {
 
-    use super::permute;
+    use super::rotate;
+    use super::rotate1;
 
     #[test]
-    fn it_permute() {
-        let mut arr = vec![1, 2, 3];
-        let exp_res = vec![
-            vec![1, 2, 3],
-            vec![1, 3, 2],
-            vec![2, 1, 3],
-            vec![2, 3, 1],
-            vec![3, 2, 1],
-            vec![3, 1, 2]
-        ];
+    fn it_rotate_arr() {
+        let mut arr = [1, 2, 3, 4, 5, 6, 7];
 
-        assert_eq!(permute(&mut arr), exp_res);
+        rotate(&mut arr, 3);
+
+        println!("{arr:?}");
+        assert_eq!(arr, [5, 6, 7, 1, 2, 3, 4])
     }
+
+    #[test]
+    fn it_rotate_arr1() {
+        let mut arr = [-1,-100,3,99];
+
+        rotate(&mut arr, 2);
+
+        println!("{arr:?}");
+        assert_eq!(arr, [3, 99, -1, -100])
+    }
+
+    #[test]
+    fn it_rotate_arr2() {
+        let mut arr = [1, 2, 3];
+
+        rotate(&mut arr, 2);
+
+        println!("{arr:?}");
+        assert_eq!(arr, [2, 3, 1])
+    }
+
+    #[test]
+    fn it_rotate1_arr() {
+        let mut arr = [1, 2, 3, 4, 5, 6, 7];
+
+        rotate1(&mut arr, 3);
+
+        println!("{arr:?}");
+        assert_eq!(arr, [5, 6, 7, 1, 2, 3, 4])
+    }
+
+    #[test]
+    fn it_rotate1_arr1() {
+        let mut arr = [-1,-100,3,99];
+
+        rotate1(&mut arr, 2);
+
+        println!("{arr:?}");
+        assert_eq!(arr, [3, 99, -1, -100])
+    }
+
+    #[test]
+    fn it_rotate1_arr2() {
+        let mut arr = [1, 2, 3];
+
+        rotate1(&mut arr, 2);
+
+        println!("{arr:?}");
+        assert_eq!(arr, [2, 3, 1])
+    }
+
+
 }
