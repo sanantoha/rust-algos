5de6edbdf89a7ed42f3b7254e6f23512ec92e430
sanantoha <sanantoha@gmail.com>
2025-12-29 07:27:42 +0400

    +1 completed.

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,51 +1,171 @@
-use crate::graph::EdgeT;
-use std::collections::HashMap;
-use std::rc::Rc;
+use crate::graph::EdgeWeightedGraph;
 
-
-pub fn dfs_rec<'a>(graph: &'a HashMap<String, Vec<Rc<EdgeT<String>>>>, start: &'a str) -> Vec<&'a str> {
-
-    vec![]
+pub fn mst(graph: &EdgeWeightedGraph) -> Option<EdgeWeightedGraph> {
+    None
 }
 
-pub fn dfs<'a>(graph: &'a HashMap<String, Vec<Rc<EdgeT<String>>>>, start: &'a str) -> Vec<&'a str> {
-    vec![]
+pub fn mst1(graph: &EdgeWeightedGraph) -> Option<EdgeWeightedGraph> {
+    None
 }
-
 
 #[cfg(test)]
 mod tests {
-    use std::path::PathBuf;
-
-    use super::dfs;
-    use super::dfs_rec;
-    use crate::graph::{graph_from_file, graph_to_string};
-
-    const PATH: &str = "src/graph/dfs.txt";
-
-    const EXP: &[&str] = &["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"];
+    use super::{mst, mst1};
+    use crate::graph::EdgeWeightedGraph;
+    use crate::graph::{compare, Edge};
 
     #[test]
-    fn it_dfs_rec() {
-        if let Ok(graph) = graph_from_file(PathBuf::from(PATH)) {
-            let graph_str = graph_to_string(&graph);
-            println!("{}", graph_str);
+    fn it_mst() {
+        /*
+        6 5
+        0: 0-1 7.00000
+        1: 1-2 3.00000  0-1 7.00000
+        2: 1-2 3.00000  2-4 3.00000
+        3: 3-4 2.00000
+        4: 3-4 2.00000  4-5 2.00000  2-4 3.00000
+        5: 4-5 2.00000
+        */
 
-            let res = dfs_rec(&graph, "0");
-            println!("{:?}", res);
-            assert_eq!(res, EXP)
-        }
+        let graph = create_graph();
+        println!("original:\n{}", graph);
+
+        let exp_graph = create_exp_graph();
+        println!("expected:\n{}", exp_graph);
+
+        let res_graph = mst(&graph).expect("should return graph");
+        println!("result:\n{}", &res_graph);
+
+        // assert_eq!(res_graph, exp_graph);
+        assert!(compare(&res_graph, &exp_graph));
     }
 
     #[test]
-    fn it_dfs() {
-        if let Ok(graph) = graph_from_file(PathBuf::from(PATH)) {
-            let graph_str = graph_to_string(&graph);
-            println!("{}", graph_str);
+    fn it_mst_case2() {
 
-            let res = dfs(&graph, "0");
-            println!("{:?}", res);
-            assert_eq!(res, EXP)
-        }
+        /*
+        7 6
+        0: 0-1 2.00000  0-2 3.00000
+        1: 0-1 2.00000  1-6 3.00000
+        2: 2-4 1.00000  0-2 3.00000
+        3: 3-4 5.00000
+        4: 2-4 1.00000  3-4 5.00000
+        5: 5-6 2.00000
+        6: 5-6 2.00000  1-6 3.00000
+
+         */
+        let graph = create_graph1();
+        println!("original:\n{}", graph);
+
+        let exp_graph = create_exp_graph1();
+        println!("expected:\n{}", exp_graph);
+
+        let res_graph = mst(&graph).expect("should return graph");
+        println!("result:\n{}", &res_graph);
+
+        // assert_eq!(res_graph, exp_graph);
+        assert!(compare(&res_graph, &exp_graph));
     }
+
+    #[test]
+    fn it_mst1() {
+        /*
+        6 5
+        0: 0-1 7.00000
+        1: 1-2 3.00000  0-1 7.00000
+        2: 1-2 3.00000  2-4 3.00000
+        3: 3-4 2.00000
+        4: 3-4 2.00000  4-5 2.00000  2-4 3.00000
+        5: 4-5 2.00000
+        */
+
+        let graph = create_graph();
+        println!("original:\n{}", graph);
+
+        let exp_graph = create_exp_graph();
+        println!("expected:\n{}", exp_graph);
+
+        let res_graph = mst1(&graph).expect("should return graph");
+        println!("result:\n{}", &res_graph);
+
+        // assert_eq!(res_graph, exp_graph);
+        assert!(compare(&res_graph, &exp_graph));
+    }
+
+    #[test]
+    fn it_mst1_case2() {
+
+        /*
+        7 6
+        0: 0-1 2.00000  0-2 3.00000
+        1: 0-1 2.00000  1-6 3.00000
+        2: 2-4 1.00000  0-2 3.00000
+        3: 3-4 5.00000
+        4: 2-4 1.00000  3-4 5.00000
+        5: 5-6 2.00000
+        6: 5-6 2.00000  1-6 3.00000
+
+         */
+        let graph = create_graph1();
+        println!("original:\n{}", graph);
+
+        let exp_graph = create_exp_graph1();
+        println!("expected:\n{}", exp_graph);
+
+        let res_graph = mst1(&graph).expect("should return graph");
+        println!("result:\n{}", &res_graph);
+
+        // assert_eq!(res_graph, exp_graph);
+        assert!(compare(&res_graph, &exp_graph));
+    }
+
+    fn create_exp_graph1() -> EdgeWeightedGraph {
+        let mut exp_graph = EdgeWeightedGraph::new(7);
+        exp_graph.add_edge(Edge::new(0, 1, 2.0));
+        exp_graph.add_edge(Edge::new(0, 2, 3.0));
+        exp_graph.add_edge(Edge::new(1, 6, 3.0));
+        exp_graph.add_edge(Edge::new(2, 4, 1.0));
+        exp_graph.add_edge(Edge::new(5, 6, 2.0));
+        exp_graph.add_edge(Edge::new(3, 4, 5.0));
+        exp_graph
+    }
+
+    fn create_graph1() -> EdgeWeightedGraph {
+        let mut graph = EdgeWeightedGraph::new(7);
+        graph.add_edge(Edge::new(0, 1, 2.0));
+        graph.add_edge(Edge::new(0, 2, 3.0));
+        graph.add_edge(Edge::new(0, 3, 7.0));
+        graph.add_edge(Edge::new(1, 2, 6.0));
+        graph.add_edge(Edge::new(1, 6, 3.0));
+        graph.add_edge(Edge::new(2, 4, 1.0));
+        graph.add_edge(Edge::new(2, 5, 8.0));
+        graph.add_edge(Edge::new(5, 6, 2.0));
+        graph.add_edge(Edge::new(3, 4, 5.0));
+        graph.add_edge(Edge::new(4, 5, 4.0));
+        graph
+    }
+
+    fn create_exp_graph() -> EdgeWeightedGraph {
+        let mut exp_graph = EdgeWeightedGraph::new(6);
+        exp_graph.add_edge(Edge::new(0, 1, 7.0));
+        exp_graph.add_edge(Edge::new(1, 2, 3.0));
+        exp_graph.add_edge(Edge::new(2, 4, 3.0));
+        exp_graph.add_edge(Edge::new(3, 4, 2.0));
+        exp_graph.add_edge(Edge::new(4, 5, 2.0));
+        exp_graph
+    }
+
+    fn create_graph() -> EdgeWeightedGraph {
+        let mut graph = EdgeWeightedGraph::new(6);
+        graph.add_edge(Edge::new(0, 1, 7.0));
+        graph.add_edge(Edge::new(0, 2, 8.0));
+        graph.add_edge(Edge::new(1, 2, 3.0));
+        graph.add_edge(Edge::new(1, 3, 6.0));
+        graph.add_edge(Edge::new(2, 3, 4.0));
+        graph.add_edge(Edge::new(2, 4, 3.0));
+        graph.add_edge(Edge::new(3, 4, 2.0));
+        graph.add_edge(Edge::new(3, 5, 5.0));
+        graph.add_edge(Edge::new(4, 5, 2.0));
+        graph
+    }
+
 }
