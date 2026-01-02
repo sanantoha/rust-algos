9463d8dae3501863725d28769c7b05d43f139639
sanantoha <sanantoha@gmail.com>
2026-01-02 07:39:24 +0400

    +1 completed.

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,40 +1,27 @@
-use std::cell::RefCell;
-use std::rc::Rc;
-use crate::list::ListNode;
 
-// O(l + r) time | O(1) space
-pub fn merge(l: &Option<Rc<RefCell<ListNode>>>, r: &Option<Rc<RefCell<ListNode>>>) -> Option<Rc<RefCell<ListNode>>> {
-
-    None
+pub fn maximum_subarray(arr: &[i32]) -> i32 {
+    0
 }
-
 
 #[cfg(test)]
 mod tests {
-
-    use crate::list::Displayable;
-    use crate::list::ListNode;
-    use super::merge;
+    use super::maximum_subarray;
 
     #[test]
-    pub fn test_merge() {
+    fn test_maximum_subarray() {
 
-        let head1 = Some(ListNode::with_next(4, Some(ListNode::with_next(8, Some(ListNode::with_next(15, Some(ListNode::new(19))))))));
-        let head2 = Some(ListNode::with_next(7, Some(ListNode::with_next(9, Some(ListNode::with_next(10, Some(ListNode::new(16))))))));
+        assert_eq!(maximum_subarray(&[3, 5, -9, 1, 3, -2, 3, 4, 7, 2, -9, 6, 3, 1, -5, 4]), 19);
+    }
 
-        let exp = Some(ListNode::with_next(4,
-                                           Some(ListNode::with_next(7,
-                                                                    Some(ListNode::with_next(8,
-                                                                                             Some(ListNode::with_next(9,
-                                                                                                                      Some(ListNode::with_next(10,
-                                                                                                                                               Some(ListNode::with_next(15,
-                                                                                                                                                                        Some(ListNode::with_next(16,
-                                                                                                                                                                                                 Some(ListNode::new(19))))))))))))))));
+    #[test]
+    fn test_maximum_subarray1() {
 
-        let res = merge(&head1, &head2);
-        if let Some(display_res) = Displayable::from_option(res.clone()) {
-            println!("{}", display_res);
-        }
-        assert_eq!(res, exp);
+        assert_eq!(maximum_subarray(&[-2,1,-3,4,-1,2,1,-5,4]), 6);
+    }
+
+    #[test]
+    fn test_maximum_subarray2() {
+
+        assert_eq!(maximum_subarray(&[3, 4, -6, 7, 8, -18, 100]), 100);
     }
 }
