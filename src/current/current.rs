8c05dd79992199c54ae6b7fe7c2f08fd6f8e68c8
sanantoha <sanantoha@gmail.com>
2026-01-09 07:39:22 +0400

    +1 completed.

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,77 +1,24 @@
-use crate::tree::TreeNode;
-
-pub fn merge_binary_trees(left: &mut Option<Box<TreeNode>>, right: &Option<Box<TreeNode>>) -> Option<Box<TreeNode>> {
-    None
+pub fn next_greater_element(arr: &[i32]) -> Vec<i32> {
+    vec![]
 }
 
-pub fn merge_binary_trees_iter(left: &mut Option<Box<TreeNode>>, right: &Option<Box<TreeNode>>) -> Option<Box<TreeNode>> {
-    None
+pub fn next_greater_element1(arr: &[i32]) -> Vec<i32> {
+    vec![]
 }
 
 #[cfg(test)]
 mod tests {
-    use itertools::assert_equal;
-    use crate::tree::TreeNode;
-    use super::{merge_binary_trees, merge_binary_trees_iter};
+    use super::*;
+
+    const ARR: &[i32] = &[2, 5, -3, -4, 6, 7, 2];
 
     #[test]
-    fn test_merge_binary_trees() {
-        let mut left = create_left_tree();
-        let right = create_right_tree();
-        let exp_tree = create_exp_tree();
-
-        assert_equal(merge_binary_trees(&mut left, &right), exp_tree);
+    fn test_next_greater_element() {
+        assert_eq!(next_greater_element(ARR), vec![5, 6, 6, 6, 7, -1, 5])
     }
 
     #[test]
-    fn test_merge_binary_trees_iter() {
-        let mut left = create_left_tree();
-        let right = create_right_tree();
-        let exp_tree = create_exp_tree();
-
-        assert_equal(merge_binary_trees_iter(&mut left, &right), exp_tree);
-    }
-
-    fn create_left_tree() -> Option<Box<TreeNode>> {
-        let root = TreeNode::new(1,
-                                 Some(Box::new(TreeNode::new(3,
-                                                             Some(Box::new(TreeNode::leaf(7))),
-                                                             Some(Box::new(TreeNode::leaf(4)))
-                                 ))),
-                                 Some(Box::new(TreeNode::leaf(2)))
-        );
-
-        Some(Box::new(root))
-    }
-
-    fn create_right_tree() -> Option<Box<TreeNode>> {
-        let root = TreeNode::new(1,
-                                 Some(Box::new(TreeNode::new(5,
-                                                             Some(Box::new(TreeNode::leaf(2))),
-                                                             None
-                                 ))),
-                                 Some(Box::new(TreeNode::new(9,
-                                                             Some(Box::new(TreeNode::leaf(7))),
-                                                             Some(Box::new(TreeNode::leaf(6)))
-                                 )))
-        );
-
-        Some(Box::new(root))
-    }
-
-    fn create_exp_tree() -> Option<Box<TreeNode>> {
-        let root = TreeNode::new(2,
-                                 Some(Box::new(TreeNode::new(8,
-                                                             Some(Box::new(TreeNode::leaf(9))),
-                                                             Some(Box::new(TreeNode::leaf(4)))
-                                 ))),
-                                 Some(Box::new(TreeNode::new(11,
-                                                             Some(Box::new(TreeNode::leaf(7))),
-                                                             Some(Box::new(TreeNode::leaf(6)))
-                                 )))
-        );
-
-        Some(Box::new(root))
+    fn test_next_greater_element1() {
+        assert_eq!(next_greater_element1(ARR), vec![5, 6, 6, 6, 7, -1, 5])
     }
 }
-
