c4eb305d5742262649a1b0d687ee31854fafb8d5
sanantoha <sanantoha@gmail.com>
2026-01-10 09:16:33 +0400

    +1 completed.

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,24 +1,103 @@
-pub fn next_greater_element(arr: &[i32]) -> Vec<i32> {
-    vec![]
+use crate::tree::TreeNode;
+
+pub fn is_symmetric_rec(root: &Option<Box<TreeNode>>) -> bool {
+    false
 }
 
-pub fn next_greater_element1(arr: &[i32]) -> Vec<i32> {
-    vec![]
+
+pub fn is_symmetric(root: &Option<Box<TreeNode>>) -> bool {
+    false
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
-    const ARR: &[i32] = &[2, 5, -3, -4, 6, 7, 2];
+    use super::{is_symmetric, is_symmetric_rec};
+    use crate::tree::TreeNode;
 
     #[test]
-    fn test_next_greater_element() {
-        assert_eq!(next_greater_element(ARR), vec![5, 6, 6, 6, 7, -1, 5])
+    fn test_is_symmetric() {
+        let root = create_tree();
+
+        assert!(is_symmetric(&root));
     }
 
     #[test]
-    fn test_next_greater_element1() {
-        assert_eq!(next_greater_element1(ARR), vec![5, 6, 6, 6, 7, -1, 5])
+    fn test_is_symmetric_rec() {
+        let root = create_tree();
+
+        assert!(is_symmetric_rec(&root));
+    }
+
+    #[test]
+    fn test_is_symmetric_case1() {
+        let root = create_tree1();
+
+        assert!(!is_symmetric(&root));
+    }
+
+    #[test]
+    fn test_is_symmetric_rec_case1() {
+        let root = create_tree1();
+
+        assert!(!is_symmetric_rec(&root));
+    }
+
+    #[test]
+    fn test_is_symmetric_case2() {
+        let root = create_tree2();
+
+        assert!(!is_symmetric(&root));
+    }
+
+    #[test]
+    fn test_is_symmetric_rec_case2() {
+        let root = create_tree2();
+
+        assert!(!is_symmetric_rec(&root));
+    }
+
+    fn create_tree() -> Option<Box<TreeNode>> {
+        let root = TreeNode::new(
+            1,
+            Some(Box::new(TreeNode::new(
+                2,
+                Some(Box::new(TreeNode::leaf(3))),
+                Some(Box::new(TreeNode::leaf(4))),
+            ))),
+            Some(Box::new(TreeNode::new(
+                2,
+                Some(Box::new(TreeNode::leaf(4))),
+                Some(Box::new(TreeNode::leaf(3))),
+            ))),
+        );
+
+        Some(Box::new(root))
+    }
+
+    fn create_tree1() -> Option<Box<TreeNode>> {
+        let root = TreeNode::new(
+            1,
+            Some(Box::new(TreeNode::new(
+                2,
+                None,
+                Some(Box::new(TreeNode::leaf(3))),
+            ))),
+            Some(Box::new(TreeNode::new(
+                2,
+                None,
+                Some(Box::new(TreeNode::leaf(3))),
+            ))),
+        );
+
+        Some(Box::new(root))
+    }
+
+    fn create_tree2() -> Option<Box<TreeNode>> {
+        // TreeNode root2 = new TreeNode(2, new TreeNode(1), new TreeNode(3));
+        let root = TreeNode::new(2,
+                                 Some(Box::new(TreeNode::leaf(1))),
+                                 Some(Box::new(TreeNode::leaf(3)))
+        );
+        Some(Box::new(root))
     }
 }
