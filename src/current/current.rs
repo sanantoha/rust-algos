08cb8af76f57c2f448fa0ab746fb79bd466408e1
sanantoha <sanantoha@gmail.com>
2026-01-12 14:00:17 +0400

    +1 completed.

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,103 +1,48 @@
-use crate::tree::TreeNode;
 
-pub fn is_symmetric_rec(root: &Option<Box<TreeNode>>) -> bool {
-    false
+
+pub fn update_matrix(matrix: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+    vec![]
 }
 
-
-pub fn is_symmetric(root: &Option<Box<TreeNode>>) -> bool {
-    false
-}
 
 #[cfg(test)]
 mod tests {
-    use super::{is_symmetric, is_symmetric_rec};
-    use crate::tree::TreeNode;
+
+    use super::update_matrix;
 
     #[test]
-    fn test_is_symmetric() {
-        let root = create_tree();
+    pub fn test_update_matrix_case1() {
+        let input = vec![
+            vec![0, 0, 0],
+            vec![0, 1, 0],
+            vec![0, 0, 0],
+        ];
+        let exp = vec![
+            vec![0, 0, 0],
+            vec![0, 1, 0],
+            vec![0, 0, 0],
+        ];
 
-        assert!(is_symmetric(&root));
-    }
+        let res = update_matrix(input);
 
-    #[test]
-    fn test_is_symmetric_rec() {
-        let root = create_tree();
-
-        assert!(is_symmetric_rec(&root));
-    }
-
-    #[test]
-    fn test_is_symmetric_case1() {
-        let root = create_tree1();
-
-        assert!(!is_symmetric(&root));
+        assert_eq!(res, exp);
     }
 
     #[test]
-    fn test_is_symmetric_rec_case1() {
-        let root = create_tree1();
+    pub fn test_update_matrix_case2() {
+        let input = vec![
+            vec![0, 0, 0],
+            vec![0, 1, 0],
+            vec![1, 1, 1],
+        ];
+        let exp = vec![
+            vec![0, 0, 0],
+            vec![0, 1, 0],
+            vec![1, 2, 1],
+        ];
 
-        assert!(!is_symmetric_rec(&root));
-    }
+        let res = update_matrix(input);
 
-    #[test]
-    fn test_is_symmetric_case2() {
-        let root = create_tree2();
-
-        assert!(!is_symmetric(&root));
-    }
-
-    #[test]
-    fn test_is_symmetric_rec_case2() {
-        let root = create_tree2();
-
-        assert!(!is_symmetric_rec(&root));
-    }
-
-    fn create_tree() -> Option<Box<TreeNode>> {
-        let root = TreeNode::new(
-            1,
-            Some(Box::new(TreeNode::new(
-                2,
-                Some(Box::new(TreeNode::leaf(3))),
-                Some(Box::new(TreeNode::leaf(4))),
-            ))),
-            Some(Box::new(TreeNode::new(
-                2,
-                Some(Box::new(TreeNode::leaf(4))),
-                Some(Box::new(TreeNode::leaf(3))),
-            ))),
-        );
-
-        Some(Box::new(root))
-    }
-
-    fn create_tree1() -> Option<Box<TreeNode>> {
-        let root = TreeNode::new(
-            1,
-            Some(Box::new(TreeNode::new(
-                2,
-                None,
-                Some(Box::new(TreeNode::leaf(3))),
-            ))),
-            Some(Box::new(TreeNode::new(
-                2,
-                None,
-                Some(Box::new(TreeNode::leaf(3))),
-            ))),
-        );
-
-        Some(Box::new(root))
-    }
-
-    fn create_tree2() -> Option<Box<TreeNode>> {
-        // TreeNode root2 = new TreeNode(2, new TreeNode(1), new TreeNode(3));
-        let root = TreeNode::new(2,
-                                 Some(Box::new(TreeNode::leaf(1))),
-                                 Some(Box::new(TreeNode::leaf(3)))
-        );
-        Some(Box::new(root))
+        assert_eq!(res, exp);
     }
 }
