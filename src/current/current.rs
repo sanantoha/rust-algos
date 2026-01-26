ca7e4e184465b19584dd361a618e1ae5b54f9c20
sanantoha <sanantoha@gmail.com>
2026-01-26 14:42:10 +0400

    +1 completed.

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,30 +1,32 @@
 use crate::tree::TreeNode;
 
-
-pub fn left_view(root: Box<TreeNode>) -> Vec<i32> {
-    vec![]
+pub fn validate(root: &Option<Box<TreeNode>>) -> bool {
+    false
 }
 
 #[cfg(test)]
 mod tests {
-    use super::left_view;
     use crate::tree::TreeNode;
+    use super::validate;
 
     #[test]
-    fn it_left_view() {
-        let root = Box::new(TreeNode::new(1,
-                                          Some(Box::new(TreeNode::leaf(2))),
-                                          Some(Box::new(TreeNode::new(3,
-                                                                      Some(Box::new(TreeNode::new(4,
-                                                                                                  Some(Box::new(TreeNode::leaf(5))),
-                                                                                                  Some(Box::new(TreeNode::new(6,
-                                                                                                                              None,
-                                                                                                                              Some(Box::new(TreeNode::leaf(7))))))))),
-                                                                      Some(Box::new(TreeNode::leaf(8)))))),
-        ));
+    fn test_validate() {
+        let root = Some(Box::new(TreeNode::new(
+            5,
+            Some(Box::new(TreeNode::new(
+                2,
+                Some(Box::new(TreeNode::leaf(1))),
+                Some(Box::new(TreeNode::leaf(3))),
+            ))),
+            Some(Box::new(TreeNode::new(
+                10,
+                Some(Box::new(TreeNode::leaf(7))),
+                Some(Box::new(TreeNode::leaf(15))),
+            ))),
+        )));
 
-        let res = left_view(root);
-        println!("{res:?}");
-        assert_eq!(res, vec![1, 2, 4, 5, 7]);
+        assert!(validate(&root));
     }
+
+
 }
