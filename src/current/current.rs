14b2333cb6f6ecd7bc78a8b85d4701a27a7232c9
sanantoha <sanantoha@gmail.com>
2026-02-05 14:22:35 +0400

    +1 completed.

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,22 +1,49 @@
+use crate::tree::TreeNode;
 
-pub fn number_of_islands(grid: &[&[i32]]) -> i32 {
-    0
+pub fn find_node_distance_k(root: &Option<Box<TreeNode>>, target: i32, k: i32) -> Vec<i32> {
+    vec![]
 }
 
+pub fn find_node_distance_k_rec(root: &Option<Box<TreeNode>>, target: i32, k: i32) -> Vec<i32> {
+    vec![]
+}
 
 #[cfg(test)]
-mod test {
-    use super::number_of_islands;
+mod tests {
+    use super::*;
 
     #[test]
-    fn test_number_of_islands() {
-        let grid: &[&[i32]] = &[
-            &[0, 0, 0, 0, 1],
-            &[1, 1, 0, 0, 0],
-            &[1, 1, 0, 1, 1],
-            &[0, 0, 0, 1, 1],
-        ];
+    fn test_find_node_distance_k() {
+        let root = create_tree();
 
-        assert_eq!(number_of_islands(grid), 3);
+        let mut res = find_node_distance_k(&root, 3, 2);
+        res.sort();
+        assert_eq!(res, vec![2, 7, 8]);
+    }
+
+    #[test]
+    fn test_find_node_distance_k_rec() {
+        let root = create_tree();
+
+        let mut res = find_node_distance_k_rec(&root, 3, 2);
+        res.sort();
+        assert_eq!(res, vec![2, 7, 8]);
+    }
+
+    fn create_tree() -> Option<Box<TreeNode>> {
+        let root = Some(Box::new(TreeNode::new(1,
+                                               Some(Box::new(TreeNode::new(2,
+                                                                           Some(Box::new(TreeNode::leaf(4))),
+                                                                           Some(Box::new(TreeNode::leaf(5)))
+                                               ))),
+                                               Some(Box::new(TreeNode::new(3,
+                                                                           None,
+                                                                           Some(Box::new(TreeNode::new(6,
+                                                                                                       Some(Box::new(TreeNode::leaf(7))),
+                                                                                                       Some(Box::new(TreeNode::leaf(8)))
+                                                                           )))
+                                               )))
+        )));
+        root
     }
 }
