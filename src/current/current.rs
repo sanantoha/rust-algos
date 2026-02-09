42866507dddae0e4281b0df7d23a0a347dc4629d
sanantoha <sanantoha@gmail.com>
2026-02-09 13:47:59 +0400

    +1 completed.

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,74 +1,23 @@
-pub fn can_finish(num_courses: usize, prerequisites: &[&[usize]]) -> bool {
-    false
-}
 
-
-pub fn can_finish1(num_courses: usize, prerequisites: &[&[usize]]) -> bool {
-    false
+pub fn binary_search(arr: &[i32], target: i32) -> i32 {
+    -1
 }
 
 
 #[cfg(test)]
 mod tests {
-    use super::{can_finish, can_finish1};
+
+    use super::binary_search;
+
+    const ARR: &[i32] = &[10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
 
     #[test]
-    fn test_can_finish_case1() {
-        let prerequisites: &[&[usize]] = &[];
-        assert!(can_finish(1, &prerequisites));
+    fn test_binary_search() {
+        assert_eq!(binary_search(ARR, 80), 7);
     }
 
     #[test]
-    fn test_can_finish_case2() {
-        let prerequisites: &[&[usize]] = &[&[1, 0]];
-        assert!(can_finish(2, &prerequisites));
-    }
-
-    #[test]
-    fn test_can_finish_case3() {
-        let prerequisites: &[&[usize]] = &[&[1, 0], &[0, 1]];
-        assert!(!can_finish(2, &prerequisites));
-    }
-
-    #[test]
-    fn test_can_finish_case4() {
-        let prerequisites: &[&[usize]] = &[&[1, 0], &[2, 1], &[3, 2], &[0, 3]];
-        assert!(!can_finish(4, &prerequisites));
-    }
-
-    #[test]
-    fn test_can_finish_case5() {
-        let prerequisites: &[&[usize]] = &[&[1, 0], &[2, 1], &[3, 2]];
-        assert!(can_finish(4, &prerequisites));
-    }
-
-    #[test]
-    fn test_can_finish1_case1() {
-        let prerequisites: &[&[usize]] = &[];
-        assert!(can_finish1(1, &prerequisites));
-    }
-
-    #[test]
-    fn test_can_finish1_case2() {
-        let prerequisites: &[&[usize]] = &[&[1, 0]];
-        assert!(can_finish1(2, &prerequisites));
-    }
-
-    #[test]
-    fn test_can_finish1_case3() {
-        let prerequisites: &[&[usize]] = &[&[1, 0], &[0, 1]];
-        assert!(!can_finish1(2, &prerequisites));
-    }
-
-    #[test]
-    fn test_can_finish1_case4() {
-        let prerequisites: &[&[usize]] = &[&[1, 0], &[2, 1], &[3, 2], &[0, 3]];
-        assert!(!can_finish1(4, &prerequisites));
-    }
-
-    #[test]
-    fn test_can_finish1_case5() {
-        let prerequisites: &[&[usize]] = &[&[1, 0], &[2, 1], &[3, 2]];
-        assert!(can_finish1(4, &prerequisites));
+    fn test_binary_search_missing_target() {
+        assert_eq!(binary_search(ARR, 81), -9);
     }
 }
