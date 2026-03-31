07fe7ec99918c8e3d4135851f28880fca57b6a77
sanantoha <sanantoha@gmail.com>
2026-03-31 21:49:05 +0400

    +1 completed.

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,58 +1,31 @@
-use crate::tree::TreeNode;
 
 
-pub fn find_closest_value_in_bst_rec(root: &Option<Box<TreeNode>>, target: i32) -> Option<i32> {
-    None
-}
-
-
-pub fn find_closest_value_in_bst(root: &Option<Box<TreeNode>>, target: i32) -> Option<i32> {
-
-    None
+pub fn reverse(i: i32) -> i32 {
+    0
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::tree::TreeNode;
-    use super::{find_closest_value_in_bst_rec, find_closest_value_in_bst};
+
+    use super::reverse;
 
     #[test]
-    fn test_find_closest_value_in_bst_rec() {
-        let root = create_tree();
-
-        assert_eq!(find_closest_value_in_bst_rec(&root, 12), Some(13));
+    fn test_reverse() {
+        assert_eq!(reverse(123), 321);
     }
 
     #[test]
-    fn test_find_closest_value_in_bst() {
-        let root = create_tree();
-
-        assert_eq!(find_closest_value_in_bst(&root, 12), Some(13));
+    fn test_reverse_case1() {
+        assert_eq!(reverse(0), 0);
     }
 
-    fn create_tree() -> Option<Box<TreeNode>> {
-        let root = TreeNode::new(
-            10,
-            Some(Box::new(TreeNode::new(
-                5,
-                Some(Box::new(TreeNode::new(
-                    2,
-                    Some(Box::new(TreeNode::leaf(1))),
-                    None,
-                ))),
-                Some(Box::new(TreeNode::leaf(5))),
-            ))),
-            Some(Box::new(TreeNode::new(
-                15,
-                Some(Box::new(TreeNode::new(
-                    13,
-                    None,
-                    Some(Box::new(TreeNode::leaf(14))),
-                ))),
-                Some(Box::new(TreeNode::leaf(22))),
-            ))),
-        );
+    #[test]
+    fn test_reverse_case2() {
+        assert_eq!(reverse(-123), -321);
+    }
 
-        Some(Box::new(root))
+    #[test]
+    fn test_reverse_case3() {
+        assert_eq!(reverse(120), 21);
     }
 }
