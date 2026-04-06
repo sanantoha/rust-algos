86fef00ab8b48194891d9884a92059f3eec6b2ff
sanantoha <sanantoha@gmail.com>
2026-04-06 16:34:46 +0400

    +1 completed.

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,31 +1,20 @@
 
-pub fn disk_stacking(disks: &[(i32, i32, i32)]) -> Vec<(i32, i32, i32)> {
-    vec![]
+pub fn subarray_sum(nums: &[i32], k: i32) -> i32 {
+    0
 }
 
 #[cfg(test)]
 mod tests {
-    use super::disk_stacking;
+
+    use super::subarray_sum;
 
     #[test]
-    fn test_disk_stacking() {
-        let disks: &[(i32, i32, i32)] = &[
-            (2, 1, 2),
-            (3, 2, 3),
-            (2, 2, 8),
-            (2, 3, 4),
-            (2, 2, 1),
-            (4, 4, 5),
-        ];
+    fn it_subarray_sum() {
+        assert_eq!(subarray_sum(&[1, 1, 1], 2), 2);
+    }
 
-        let exp: Vec<(i32, i32, i32)> = vec![
-            (2, 1, 2),
-            (3, 2, 3),
-            (4, 4, 5)
-        ];
-
-        let res = disk_stacking(disks);
-        println!("{:?}", res);
-        assert_eq!(res, exp);
+    #[test]
+    fn it_subarray_sum_case2() {
+        assert_eq!(subarray_sum(&[1, 2, 3], 3), 2);
     }
 }
