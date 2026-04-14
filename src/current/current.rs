fe1275142341d1d128c06cae16230a14c309f9b9
sanantoha <sanantoha@gmail.com>
2026-04-14 13:29:41 +0400

    +1 completed.

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,81 +1,36 @@
-use crate::list::ListNode;
-use std::cell::RefCell;
-use std::rc::Rc;
+use crate::tree::TreeNode;
 
-pub fn is_cycle(head: &Option<Rc<RefCell<ListNode>>>) -> bool {
-    false
-}
+pub fn bfs(root: &Option<Box<TreeNode>>) -> Vec<i32> {
 
-
-pub fn is_cycle2(head: &Rc<RefCell<ListNode>>) -> bool {
-    false
-}
-
-pub fn is_cycle_ws(head: &Option<Rc<RefCell<ListNode>>>) -> bool {
-    false
-}
-
-pub fn is_cycle_ws2(head: &Rc<RefCell<ListNode>>) -> bool {
-    false
+    vec![]
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{is_cycle, is_cycle2, is_cycle_ws, is_cycle_ws2};
-    use crate::list::ListNode;
-    use std::cell::RefCell;
-    use std::rc::Rc;
+    use super::bfs;
+    use crate::tree::TreeNode;
 
     #[test]
-    pub fn test_is_cycle_if_circle() {
-        let root = Some(create_circle_list());
+    pub fn test_bfs() {
 
-        assert!(is_cycle(&root));
-    }
+        let root = Some(Box::new(TreeNode::new(
+            5,
+            Some(Box::new(TreeNode::new(
+                2,
+                Some(Box::new(TreeNode::leaf(1))),
+                Some(Box::new(TreeNode::leaf(3))),
+            ))),
+            Some(Box::new(TreeNode::new(
+                10,
+                Some(Box::new(TreeNode::leaf(7))),
+                Some(Box::new(TreeNode::new(
+                    15,
+                    Some(Box::new(TreeNode::leaf(13))),
+                    Some(Box::new(TreeNode::leaf(17))),
+                ))),
+            ))),
+        )));
 
-    #[test]
-    pub fn test_is_cycle_if_circle2() {
-        let root = create_circle_list();
-
-        assert!(is_cycle2(&root));
-    }
-
-    #[test]
-    pub fn test_is_cycle_ws_if_circle() {
-        let root = Some(create_circle_list());
-
-        assert!(is_cycle_ws(&root));
-    }
-
-    #[test]
-    pub fn test_is_cycle_ws_if_circle2() {
-        let root = create_circle_list();
-
-        assert!(is_cycle_ws2(&root));
-    }
-
-    #[test]
-    pub fn test_is_not_cycle() {
-        let root = Some(create_list());
-        assert!(!is_cycle(&root));
-    }
-
-    #[test]
-    pub fn test_ic_cycle_ws() {
-        let root = Some(create_list());
-        assert!(!is_cycle_ws(&root));
-    }
-
-    pub fn create_circle_list() -> Rc<RefCell<ListNode>> {
-        let node0 = ListNode::with_next(0, Some(ListNode::with_next(1, Some(ListNode::new(3)))));
-        let node4 = ListNode::with_next(4, Some(ListNode::with_next(5, Some(ListNode::with_next(6, Some(ListNode::with_next(7, Some(Rc::clone(&node0)))))))));
-
-        node0.borrow().next.as_ref().unwrap().borrow().next.as_ref().unwrap().borrow_mut().next = Some(Rc::clone(&node4));
-
-        node0
-    }
-
-    pub fn create_list() -> Rc<RefCell<ListNode>> {
-        ListNode::with_next(0, Some(ListNode::with_next(1, Some(ListNode::new(2)))))
+        assert_eq!(bfs(&root), vec![5, 2, 10, 1, 3, 7, 15, 13, 17]);
     }
 }
