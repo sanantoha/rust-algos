b4b611882c2c33fee1151a59ecf32243cd224fc7
sanantoha <sanantoha@gmail.com>
2026-04-23 17:07:47 +0400

    +1 completed.

--- src/current/current.rs
+++ src/current/current.rs
@@ -1,36 +1,82 @@
-use crate::tree::TreeNode;
+use std::cell::RefCell;
+use std::rc::Rc;
+use std::{fmt, io};
 
-pub fn bfs(root: &Option<Box<TreeNode>>) -> Vec<i32> {
-
-    vec![]
+#[allow(dead_code)]
+#[derive(Debug)]
+struct LRUCache {
+    capacity: usize,
 }
+
+#[allow(dead_code)]
+impl LRUCache {
+
+    pub fn new(capacity: usize) -> Result<LRUCache, Box<dyn std::error::Error>> {
+        let err = std::io::Error::new(io::ErrorKind::InvalidInput, "capacity should be greater that zero");
+        Err(Box::new(err))
+    }
+
+    pub fn get(&self, key: i32) -> Option<i32> {
+        None
+    }
+
+    pub fn put(&mut self, key: i32, val: i32) {
+
+    }
+}
+
+impl fmt::Display for LRUCache {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+//         let head = &self.head.borrow().next.as_ref().map(|x| format!("{}", x.borrow())).unwrap_or("".to_owned());
+//         let tail = &self.tail.borrow().prev.as_ref().map(|x| format!("{}", x.borrow())).unwrap_or("".to_owned());
+//         write!(f, "LRUCache {{
+//     capacity: {},
+//     head: {},
+//     tail: {}
+// }}", &self.capacity, head, tail)
+        Ok(())
+    }
+}
+
 
 #[cfg(test)]
 mod tests {
-    use super::bfs;
-    use crate::tree::TreeNode;
+    use super::LRUCache;
 
     #[test]
-    pub fn test_bfs() {
+    fn it_lru_cache() {
 
-        let root = Some(Box::new(TreeNode::new(
-            5,
-            Some(Box::new(TreeNode::new(
-                2,
-                Some(Box::new(TreeNode::leaf(1))),
-                Some(Box::new(TreeNode::leaf(3))),
-            ))),
-            Some(Box::new(TreeNode::new(
-                10,
-                Some(Box::new(TreeNode::leaf(7))),
-                Some(Box::new(TreeNode::new(
-                    15,
-                    Some(Box::new(TreeNode::leaf(13))),
-                    Some(Box::new(TreeNode::leaf(17))),
-                ))),
-            ))),
-        )));
+        if let Ok(mut lru_cache) = LRUCache::new(2) {
+            lru_cache.put(1, 1);
+            lru_cache.put(2, 2);
+            println!("{}", lru_cache);
 
-        assert_eq!(bfs(&root), vec![5, 2, 10, 1, 3, 7, 15, 13, 17]);
+            let r = lru_cache.get(1);
+            println!("{:?}", r); // 1
+            assert_eq!(r, Some(1));
+            println!("{}", lru_cache);
+
+
+            lru_cache.put(3, 3);
+            println!("{}", lru_cache);
+
+            let r = lru_cache.get(2);
+            println!("{:?}", r); // None
+            assert_eq!(r, None);
+
+            lru_cache.put(4, 4);
+            println!("{}", lru_cache);
+
+            let r = lru_cache.get(1);
+            println!("{:?}", r); // None
+            assert_eq!(r, None);
+            let r = lru_cache.get(3);
+            println!("{:?}", r); // Some(3)
+            assert_eq!(r, Some(3));
+            let r = lru_cache.get(4);
+            println!("{:?}", r); // Some(4)
+            assert_eq!(r, Some(4));
+            println!("{}", lru_cache);
+        }
     }
 }
